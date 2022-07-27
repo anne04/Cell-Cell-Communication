a6bb17f145a1f24916a734513be96812e558631e
anne04 <anne.06.cse@gmail.com>
2022-07-27 13:17:01 -0400

    Update test_ccst.py

--- test_ccst.py
+++ test_ccst.py
@@ -1,1 +1,31 @@
+import os
+import sys
+import matplotlib
+matplotlib.use('Agg')
+#matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pylab as pl
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+from sklearn import metrics
+from scipy import interp
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+
+import numpy as np
+from scipy import sparse
+import pickle
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, ChebConv, GATConv, DeepGraphInfomax, global_mean_pool, global_max_pool  # noqa
+from torch_geometric.data import Data, DataLoader
+
+from CCST import get_graph, train_DGI, train_DGI, PCA_process, Kmeans_cluster
+
+rootPath = os.path.dirname(sys.path[0])
+os.chdir(rootPath+'/CCST')
 
