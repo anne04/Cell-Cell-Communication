d2f1cd4764faa365f734b93c32f8fb59cffe05f8
anne04 <anne.06.cse@gmail.com>
2022-08-10 17:00:39 -0400

    Update transpose_node_embedding.py.py

--- transpose_node_embedding.py.py
+++ transpose_node_embedding.py.py
@@ -1,18 +1,7 @@
 import numpy as np
 import csv
 import pickle
-
-#X_embedding_filename =  args.embedding_data_path+'lambdaI' + str(lambda_I) + '_epoch' + str(args.num_epoch) + '_Embed_X.npy'
-X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI1.0_Embed_X.npy'
-X_embedding = np.load(X_embedding_filename)
-X_embedding_T = np.transpose(X_embedding)
-
-X_gene_data_path = '/cluster/projects/schwartzgroup/fatema/CCST/generated_data_noPCA_QuantileTransform/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/'
-fp = open(X_gene_data_path + 'features', 'rb')
-X_gene_data = pickle.load(fp)
-fp.close()
-#X_gene_data = np.load(X_gene_data_path)
-X_gene_data_T = np.transpose(X_gene_data)
+from scipy import sparse
 
 barcode_file='/cluster/home/t116508uhn/64630/barcodes.tsv'
 barcode_info=[]
@@ -21,14 +10,13 @@ with open(barcode_file) as file:
     for line in tsv_file:
         barcode_info.append(line[0])
 
-        
-X_embedding_filename = '/cluster/home/t116508uhn/64630/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_Embed_X.csv'
-f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
-writer = csv.writer(f)
-# write the header
-writer.writerow(barcode_info)
-writer.writerows(X_embedding_T)
-f.close()
+X_gene_data_path = '/cluster/projects/schwartzgroup/fatema/CCST/generated_data_noPCA_QuantileTransform/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/'
+fp = open(X_gene_data_path + 'features', 'rb')
+X_gene_data = pickle.load(fp)
+fp.close()
+#X_gene_data = np.load(X_gene_data_path)
+X_gene_data=scipy.sparse.csr_matrix(X_gene_data)
+X_gene_data_T = np.transpose(X_gene_data)
 
 X_gene_filename = '/cluster/home/t116508uhn/64630/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_X_gene.csv'
 f=open(X_gene_filename, 'w', encoding='UTF8', newline='') #'/cluster/home/t116508uhn/test.csv'
@@ -37,6 +25,23 @@ writer = csv.writer(f)
 writer.writerow(barcode_info)
 writer.writerows(X_gene_data_T)
 f.close()
+
+
+
+#X_embedding_filename =  args.embedding_data_path+'lambdaI' + str(lambda_I) + '_epoch' + str(args.num_epoch) + '_Embed_X.npy'
+X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI1.0_Embed_X.npy'
+X_embedding = np.load(X_embedding_filename)
+X_embedding_T = np.transpose(X_embedding)
+       
+X_embedding_filename = '/cluster/home/t116508uhn/64630/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_Embed_X.csv'
+f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
+writer = csv.writer(f)
+# write the header
+writer.writerow(barcode_info)
+writer.writerows(X_embedding_T)
+f.close()
+
+
 
 
 barcode_label_file='/cluster/home/t116508uhn/64630/node_label.csv'
