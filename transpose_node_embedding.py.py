dce31c33d7eb615fa1e5d47378f87431af916a0e
anne04 <anne.06.cse@gmail.com>
2022-08-10 11:43:43 -0400

    Update transpose_node_embedding.py.py

--- transpose_node_embedding.py.py
+++ transpose_node_embedding.py.py
@@ -1,25 +1,39 @@
 import numpy as np
 import csv
+
 #X_embedding_filename =  args.embedding_data_path+'lambdaI' + str(lambda_I) + '_epoch' + str(args.num_epoch) + '_Embed_X.npy'
 X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_epoch10000_Embed_X.npy'
 X_embedding = np.load(X_embedding_filename)
-
 X_embedding_T = np.transpose(X_embedding)
-X_embedding_filename = '/cluster/home/t116508uhn/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_Embed_X.csv'
-np.savetxt(X_embedding_filename, X_embedding_T, delimiter=",")
 
+X_gene_data_path = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_epoch10000_Embed_X.npy'
+X_gene_data = np.load(X_gene_data_path)
+X_gene_data_T = np.transpose(X_gene_data)
 
-import csv
 barcode_file='/cluster/home/t116508uhn/64630/barcodes.tsv'
 barcode_info=[]
 with open(barcode_file) as file:
     tsv_file = csv.reader(file, delimiter="\t")
     for line in tsv_file:
-        barcode_info.append(line)
+        barcode_info.append(line[0])
+
         
+X_embedding_filename = '/cluster/home/t116508uhn/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_Embed_X.csv'
+f=open('/cluster/home/t116508uhn/test.csv', 'w', encoding='UTF8', newline='')
+writer = csv.writer(f)
+
+# write the header
+writer.writerow(barcode_info)
+writer.writerows(X_embedding_T)
+
+f.close()
+
 barcode_label_file='/cluster/home/t116508uhn/64630/node_label.csv'
 barcode_label_info=[]
 with open(barcode_label_file) as file:
     tsv_file = csv.reader(file, delimiter=",")
     for line in tsv_file:
         barcode_label_info.append(line)
+        
+        
+     
