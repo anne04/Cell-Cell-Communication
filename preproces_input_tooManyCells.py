92261091fd0231853b21efbfc33824045dd2b7be
anne04 <anne.06.cse@gmail.com>
2022-08-11 13:23:40 -0400

    Update preproces_input_tooManyCells.py

--- preproces_input_tooManyCells.py
+++ preproces_input_tooManyCells.py
@@ -37,7 +37,7 @@ f.close()
 
 #X_embedding_filename =  args.embedding_data_path+'lambdaI' + str(lambda_I) + '_epoch' + str(args.num_epoch) + '_Embed_X.npy'
 #X_embedding_filename_2 = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI1.0_Embed_X.npy'
-X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI1.0_Embed_X.npy'
+X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_Embed_X.npy'
 X_embedding = np.load(X_embedding_filename)
 X_embedding_T = np.transpose(X_embedding)
 
@@ -49,12 +49,12 @@ writer.writerow(barcode_info)
 writer.writerows(X_embedding_T)
 f.close()
 ##############################################
-X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI1.0_Embed_X.npy'
+X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_Embed_X.npy'
 X_embedding = np.load(X_embedding_filename)
-X_embedding= sc.pp.pca(X_embedding, n_comps=200) 
+X_embedding= sc.pp.pca(X_embedding, n_comps=30) 
 
 X_embedding_T = np.transpose(X_embedding)
-X_embedding_filename = '/cluster/home/t116508uhn/64630/dummy_Embed_X_2.csv'
+X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_Embed_X.csv'
 f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
 writer = csv.writer(f)
 # write the header
@@ -64,8 +64,26 @@ f.close()
 
 ##############################################
 
+cluster_label_file='/cluster/projects/schwartzgroup/fatema/CCST/result_NoPCA_512_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/cluster_label.csv'
+cluster_label_info=[]
+with open(cluster_label_file) as file:
+    csv_file = csv.reader(file, delimiter=",")
+    for line in csv_file:
+        cluster_label_info.append(line)
 
+node_label=[]
+for i in range (0, len(cluster_label_info)):
+    node_label.append([barcode_info[i],int(cluster_label_info[i][1])])
+    
+node_label_file='/cluster/home/t116508uhn/64630/node_label.csv'
+f=open(node_label_file, 'w', encoding='UTF8', newline='')
+writer = csv.writer(f)
+# write the header
+writer.writerow(['item','label'])
+writer.writerows(node_label)
+f.close()
 
+###############################################
 barcode_label_file='/cluster/home/t116508uhn/64630/node_label.csv'
 barcode_label_info=[]
 with open(barcode_label_file) as file:
