c2f4f4c7e55c9566ad3bf11286e2ed1a137183ab
anne04 <anne.06.cse@gmail.com>
2022-08-15 12:51:30 -0400

    Update spaceranger_kmeans_plot.py

--- spaceranger_kmeans_plot.py
+++ spaceranger_kmeans_plot.py
@@ -9,7 +9,9 @@ matplotlib.use('Agg')
 #matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-kmeans_label_file='64630/Tumur_64630_K-Means_7.csv'
+
+############
+kmeans_label_file='/cluster/home/t116508uhn/64630/Tumur_64630_K-Means_7.csv'
 kmeans_label=[]
 with open(kmeans_label_file) as file:
     csv_file = csv.reader(file, delimiter=",")
@@ -22,6 +24,9 @@ for i in range (1, len(kmeans_label)):
   barcode_label[kmeans_label[i][0]] = int(kmeans_label[i][1].split('_')[0].split(' ')[1])
   if max < int(kmeans_label[i][1].split('_')[0].split(' ')[1]):
       max = int(kmeans_label[i][1].split('_')[0].split(' ')[1])
+############
+
+
 
 coordinates = np.load('/cluster/projects/schwartzgroup/fatema/CCST/generated_data_noPCA_QuantileTransform_wighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/'+'coordinates.npy')
 barcode_file='/cluster/home/t116508uhn/64630/barcodes.tsv'
@@ -50,7 +55,7 @@ for label_i in range (0, max+1):
                 count=count+1
             
                 
-    plt.scatter(x=np.array(x_index), y=-np.array(y_index), label = label_i)
+    plt.scatter(x=np.array(x_index), y=-np.array(y_index), label = label_i, s=5)
     
 print(count)
 plt.legend()
@@ -59,3 +64,57 @@ save_path = '/cluster/home/t116508uhn/64630/'
 plt.savefig(save_path+'kmeans_spaceranger_plot.png', dpi=400)
 plt.clf()
 # 413 == barcode not found, 443 = not labeled
+
+############################################################################################################
+toomany_label_file='/cluster/home/t116508uhn/64630/PCA_64embedding_pathologist_label_l1mp5_temp.csv'
+toomany_label=[]
+with open(toomany_label_file) as file:
+    csv_file = csv.reader(file, delimiter=",")
+    for line in csv_file:
+        toomany_label.append(line)
+
+barcode_label=dict()
+cluster_dict=dict()
+max=0
+for i in range (1, len(toomany_label)):
+    if len(toomany_label[i])>0 :
+        barcode_label[toomany_label[i][0]] = int(toomany_label[i][1])
+        cluster_dict[int(toomany_label[i][1])]=1
+############
+cluster_label=list(cluster_dict.keys())
+coordinates = np.load('/cluster/projects/schwartzgroup/fatema/CCST/generated_data_noPCA_QuantileTransform_wighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/'+'coordinates.npy')
+barcode_file='/cluster/home/t116508uhn/64630/barcodes.tsv'
+barcode_info=[]
+#barcode_info.append("")
+i=0
+with open(barcode_file) as file:
+    tsv_file = csv.reader(file, delimiter="\t")
+    for line in tsv_file:
+        barcode_info.append([line[0], coordinates[i,0],coordinates[i,1],0])
+        i=i+1
+count=0   
+for i in range (0, len(barcode_info)):
+    if barcode_info[i][0] in barcode_label:
+        barcode_info[i][3] = barcode_label[barcode_info[i][0]]
+    else:
+        count=count+1
+        
+print(count)
+        
+     
+for j in range (0, len(cluster_label)):
+    label_i=cluster_label[j]
+    x_index=[]
+    y_index=[]
+    for i in range (0, len(barcode_info)):
+        if barcode_info[i][3] == label_i:
+            x_index.append(barcode_info[i][1])
+            y_index.append(barcode_info[i][2])
+         
+    plt.scatter(x=np.array(x_index), y=-np.array(y_index), label = j, s=5)
+    
+plt.legend()
+
+save_path = '/cluster/home/t116508uhn/64630/'
+plt.savefig(save_path+'toomanycells_PCA_64embedding_pathologist_label_l1mp5_temp_plot.png', dpi=400)
+plt.clf()
