681b5abf59408e5827e5c1e784c95f259e3157fe
anne04 <anne.06.cse@gmail.com>
2022-08-17 00:54:50 -0400

    Update preproces_input_tooManyCells.py

--- preproces_input_tooManyCells.py
+++ preproces_input_tooManyCells.py
@@ -13,6 +13,38 @@ with open(barcode_file) as file:
     for line in tsv_file:
         barcode_info.append(line[0])
 
+##############################################
+emb_dim= 64 #512
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_melow/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_mehigh/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.8_Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_r2/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_equal/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1_mp05/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1_mp2/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1mp5_r3/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
+#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1mp5_bulk/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/r6_Embed_X.npy'
+X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1mp5_temp/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/r1_Embed_X.npy'
+
+X_embedding = np.load(X_embedding_filename)
+X_embedding= sc.pp.pca(X_embedding, n_comps=60) 
+
+X_embedding_T = np.transpose(X_embedding)
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_equal.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1_mp05.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1_mp2.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_melow.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_mehigh.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_bulk.csv'
+#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1mp5_r3.csv'
+X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1mp5_temp.csv'
+f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
+writer = csv.writer(f)
+# write the header
+writer.writerow(barcode_info)
+writer.writerows(X_embedding_T)
+f.close()
 ####################################################
 
         
@@ -42,37 +74,6 @@ X_embedding = np.load(X_embedding_filename)
 X_embedding_T = np.transpose(X_embedding)
 
 X_embedding_filename = '/cluster/home/t116508uhn/64630/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x_'+str(emb_dim)+'Embed_X_l1mp5.csv'
-f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
-writer = csv.writer(f)
-# write the header
-writer.writerow(barcode_info)
-writer.writerows(X_embedding_T)
-f.close()
-##############################################
-emb_dim= 64 #512
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_melow/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.3_Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_mehigh/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/lambdaI0.8_Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_r2/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_equal/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1_mp05/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1_mp2/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-#X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1mp5_r3/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-X_embedding_filename = '/cluster/projects/schwartzgroup/fatema/CCST/Embedding_data_NoPCA_'+str(emb_dim)+'_quantile_weighted_TDistance_2k_l1mp5_temp/V10M25-61_D1_PDA_64630_Pa_P_Spatial10x/Embed_X.npy'
-
-X_embedding = np.load(X_embedding_filename)
-X_embedding= sc.pp.pca(X_embedding, n_comps=30) 
-
-X_embedding_T = np.transpose(X_embedding)
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_equal.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1_mp05.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1_mp2.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_melow.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_mehigh.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_r2.csv'
-#X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1mp5_r3.csv'
-X_embedding_filename = '/cluster/home/t116508uhn/64630/PCA_'+str(emb_dim)+'Embed_X_l1mp5_temp.csv'
 f=open(X_embedding_filename, 'w', encoding='UTF8', newline='')
 writer = csv.writer(f)
 # write the header
