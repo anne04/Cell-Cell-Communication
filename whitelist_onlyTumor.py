60ef52cf06470891d7ab2bf7af51161b7141a3f1
anne04 <anne.06.cse@gmail.com>
2022-08-25 16:26:37 -0400

    Update whitelist_onlyTumor.py

--- whitelist_onlyTumor.py
+++ whitelist_onlyTumor.py
@@ -1,4 +1,4 @@
-
+ 
 import numpy as np
 import csv
 import pickle
@@ -9,7 +9,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-pathologist_label_file='/cluster/home/t116508uhn/64630/IX_annotation_artifacts.csv' # tumor_64630_D1_IX_annotation.csv'
+pathologist_label_file='/cluster/home/t116508uhn/64630/tumor_64630_D1_IX_annotation.csv' # IX_annotation_artifacts.csv' #
 pathologist_label=[]
 with open(pathologist_label_file) as file:
     csv_file = csv.reader(file, delimiter=",")
@@ -21,6 +21,19 @@ for i in range (1, len(pathologist_label)):
   if pathologist_label[i][1] == 'tumor': #'Tumour':
       barcode_label.append([pathologist_label[i][0]])
       
+'''barcode_file='/cluster/home/t116508uhn/64630/spaceranger_output_new/unzipped/barcodes.tsv'
+barcode_info=dict()
+i=0
+with open(barcode_file) as file:
+    tsv_file = csv.reader(file, delimiter="\t")
+    for line in tsv_file:
+        barcode_info[line[0]] = 1
+        i=i+1
+        
+for i in range (0, len(barcode_label)):   
+    if barcode_label[i][0] not in barcode_info:
+        print('not found!')'''
+    
 barcode_filename = '/cluster/home/t116508uhn/64630/tumor_whitelist.csv'
 f=open(barcode_filename, 'w', encoding='UTF8', newline='')
 writer = csv.writer(f)
