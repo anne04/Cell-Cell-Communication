5c5b681696cd7703121f83cc2aa60407b8921ce1
anne04 <anne.06.cse@gmail.com>
2022-09-01 22:41:14 -0400

    Update diff_org.py

--- diff_org.py
+++ diff_org.py
@@ -5,18 +5,19 @@
 - Node 15 :: Top right Classical A
 - Node 13 :: Bottom and Top right Classical A'''
 
-#+name: in-files-sig
+'''#+name: in-files-sig
 # - ./differential_analysis/differential_TAGConv_test_r4_13_59_org_whitelist.csv"
 # - ./differential_analysis/differential_TAGConv_test_r4_10_59_org_whitelist.csv
 # - ./differential_analysis/differential_TAGConv_test_r4_14_15_org_whitelist.csv
+'''
 
-* Intersection
+#* Intersection
 #+name: find-intersection
 #+begin_src python :results output :var inFilesSig=in-files-sig
   import os
-  import glob
+  #import glob
   import pandas as pd
-  import shutil
+  #import shutil
   import numpy as np
   import sys
   import scikit_posthocs as post
@@ -65,27 +66,31 @@
     pd.set_option('display.max_columns', 50)
 
     signatureFile = "/cluster/home/t116508uhn/64630/GeneList_KF_22Aug10.csv"
-    nodes = "13_59"
-    inFilesSig= "/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_"+nodes+"_org_whitelist.csv" #"./differential_analysis/differential_TAGConv_test_r4_13_59_org_whitelist.csv"
-    outFile_1 = "/cluster/home/t116508uhn/64630/intersection_TAGConv_test_r4_"+nodes+"_org_whitelist.csv"
-    outFile_2 = "/cluster/project/schwartz/fatema/intersection/64630/intersection_TAGConv_test_r4_"+nodes+"_org_whitelist.csv"
+    nodes = ["10_13", "10_59", "10_73", "10_86", "13_59", "13_73", "13_86", "14_15", "59_73", "59_86", "73_86"] 
+    for i in range (0, len(nodes)):
+        print(nodes[i])
+        inFilesSig= "/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_"+nodes[i]+"_org_whitelist.csv" #"./differential_analysis/differential_TAGConv_test_r4_13_59_org_whitelist.csv"
+        outFile_1 = "/cluster/home/t116508uhn/64630/intersection_TAGConv_test_r4_"+nodes[i]
+        outFile_2 = "/cluster/projects/schwartzgroup/fatema/intersection/64630/intersection_TAGConv_test_r4_"+nodes[i]
+        #dfs = map(processFile, [x for xs in inFilesSig for x in xs])
+        dfs = processFile(inFilesSig)
+        sDf = processSignatureFile(signatureFile)
 
-    
-    #dfs = map(processFile, [x for xs in inFilesSig for x in xs])
-    dfs = processFile(inFilesSig)
-    sDf = processSignatureFile(signatureFile)
+        #df = pd.concat(map(lambda x: intersect(x, sDf), dfs))
+        df = intersect(dfs, sDf)
+        resDf = summarizeSig(df)
+        # resDf = df.sort_values("log2FC", ascending=False)
 
-    #df = pd.concat(map(lambda x: intersect(x, sDf), dfs))
-    df = intersect(dfs, sDf)
-    resDf = summarizeSig(df)
-    # resDf = df.sort_values("log2FC", ascending=False)
+        print(resDf[0])
+        print(resDf[1])
 
-    print(resDf[0])
-    print(resDf[1])
 
-    
-    resDf.to_csv(outFile_1, index = False)
-    resDf.to_csv(outFile_2, index = False)
+        resDf[0].to_csv(outFile_1+"_meanRes"+"_org_whitelist.csv", index = False)
+        resDf[0].to_csv(outFile_2+"_meanRes"+"_org_whitelist.csv", index = False)
+
+        resDf[1].to_csv(outFile_1+"_statRes"+"_org_whitelist.csv", index = False)
+        resDf[1].to_csv(outFile_2+"_statRes"+"_org_whitelist.csv", index = False)
+        
 
     # print(outFile)
 
