b60f13b140f2547b3a2ec0302366b7b2b71e5d31
anne04 <anne.06.cse@gmail.com>
2022-09-15 17:24:05 -0400

    Update find_prerank.py

--- find_prerank.py
+++ find_prerank.py
@@ -29,21 +29,6 @@ from collections import defaultdict
 
 print("hello world!")
 
-def read_h5(f, i=0):
-    print("hello world! read_h5")
-    for k in f.keys():
-        if isinstance(f[k], Group):
-            print('Group', f[k])
-            print('-'*(10-5*i))
-            read_h5(f[k], i=i+1)
-            print('-'*(10-5*i))
-        elif isinstance(f[k], Dataset):
-            print('Dataset', f[k])
-            print(f[k][()])
-        else:
-            print('Name', f[k].name)
-    print("hello world! read_h5_done")
-
 #if __name__ == "__main__":
 import argparse
 parser = argparse.ArgumentParser()
@@ -57,56 +42,56 @@ args = parser.parse_args()
 
 def main(args):
     print("hello world! main")  
-    name_list = ['10_48_59_86_13']
+    name_list = ['13_48_59_86_10', '10_48_59_86_13']
     n_index = 0
-    toomany_label_file = '/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_'+name_list[n_index]+'_prerank.csv'
-    
-    gene_dict=defaultdict(list)
-    with open(toomany_label_file) as file:
-        csv_file = csv.reader(file, delimiter=",")
-        i = 0
-        for line in csv_file:
-            if i == 0 or np.float64(line[1]) == np.inf or -np.inf:
+    for n_index in range (0, len(name_list)):
+        toomany_label_file = '/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_'+name_list[n_index]+'_prerank.csv'
+
+        gene_dict=defaultdict(list)
+        with open(toomany_label_file) as file:
+            csv_file = csv.reader(file, delimiter=",")
+            i = 0
+            for line in csv_file:
+                if i == 0 or np.float64(line[1]) == np.inf or np.float64(line[1]) == -np.inf:
+                    i = i+1
+                    continue
+                gene_dict[line[0]].append(np.float64(line[1]))
                 i = i+1
-                continue
-            gene_dict[line[0]].append(np.float64(line[1]))
-            i = i+1
-            
-    for gene in gene_dict:
-        gene_dict[gene]=np.mean(gene_dict[gene])
-        
-    signature_file='/cluster/home/t116508uhn/64630/GeneList_KF_22Aug10.csv' # 1406
-    signature_info=defaultdict(list)
-    #barcode_info.append("")
-    with open(signature_file) as file:
-        tsv_file = csv.reader(file, delimiter=",")
-        for line in tsv_file:
-            if (line[0].find('Basal') > -1) or (line[0].find('Classical') > -1) :
-                signature_info[line[0]].append(line[1])
-    
-    signature_info=dict(signature_info)
 
-    data_rnk=pd.DataFrame.from_dict(gene_dict, orient='index')
+        for gene in gene_dict:
+            gene_dict[gene]=np.mean(gene_dict[gene])
 
-    pre_res = gp.prerank(rnk = data_rnk,
-                         gene_sets = signature_info,
-                         threads=4,
-                         min_size=5,
-                         max_size=1000,
-                         permutation_num=1000, # reduce number to speed up testing
-                         outdir=None, # don't write to disk
-                         seed=6,
-                         #verbose=True, # see what's going on behind the scenes
-                        )
-    print(pre_res.res2d)
-    
-    name_str='/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_'+name_list[i]+'_prerank.svg'
+        signature_file='/cluster/home/t116508uhn/64630/GeneList_KF_22Aug10.csv' # 1406
+        signature_info=defaultdict(list)
+        #barcode_info.append("")
+        with open(signature_file) as file:
+            tsv_file = csv.reader(file, delimiter=",")
+            for line in tsv_file:
+                if (line[0].find('Basal') > -1) or (line[0].find('Classical') > -1) :
+                    signature_info[line[0]].append(line[1])
 
-    terms = pre_res.res2d.Term
-    for i in range (0, 6):
-        # save figure
-        # gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname=save_path+name_str+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
-        gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname=save_path+name_str+'_'+str(i)+'_prerank_GCN_r4.svg', **pre_res.results[terms[i]])
+        signature_info=dict(signature_info)
+
+        data_rnk=pd.DataFrame.from_dict(gene_dict, orient='index')
+
+        pre_res = gp.prerank(rnk = data_rnk,
+                             gene_sets = signature_info,
+                             threads=4,
+                             min_size=5,
+                             max_size=1000,
+                             permutation_num=1000, # reduce number to speed up testing
+                             outdir=None, # don't write to disk
+                             seed=6,
+                             verbose=True, # see what's going on behind the scenes
+                            )
+        print(pre_res.res2d)
+        pre_res.res2d.to_csv('/cluster/home/t116508uhn/64630/'+name_list[n_index]+'_prerank_tagconv_test_r4.csv')
+
+        terms = pre_res.res2d.Term
+        for i in range (0, 6):
+            # save figure
+            # gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname=save_path+name_str+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
+            gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname='/cluster/home/t116508uhn/64630/'+name_list[n_index]+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
 
 
       
