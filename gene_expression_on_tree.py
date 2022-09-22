ec469e83e0176e9d18d367c0f819d4ebd5725f64
anne04 <anne.06.cse@gmail.com>
2022-09-22 13:36:55 -0400

    Update gene_expression_on_tree.py

--- gene_expression_on_tree.py
+++ gene_expression_on_tree.py
@@ -1,4 +1,4 @@
-import pandas as pd
+s import pandas as pd
 import scanpy as sc
 import numpy as np
 import stlearn as st
@@ -23,7 +23,7 @@ from collections import defaultdict
 ####################  get the whole training dataset
 
   
-signature_file='/cluster/home/t116508uhn/64630/GeneList_KF_22Aug10.csv' # 1406
+signature_file='/cluster/home/t116508uhn/64630/Geneset_22Sep21_Subtypesonly.csv' # 1406
 signature_info=defaultdict(list)
 #barcode_info.append("")
 with open(signature_file) as file:
@@ -34,14 +34,25 @@ with open(signature_file) as file:
 
 signature_info=dict(signature_info)
 #############################
+signature_list = list(signature_info.keys())
 
-for signature in signature_info.keys():
+for j in range (0, len(signature_list)):
+    signature = signature_list[j]
     gene_list=signature_info[signature]
-    print(signature+"\n")
-    for gene in gene_list:
-        print(" \\\""+ gene +"\\\",", end = ' ')
-
-
+    print("\n"+signature)
+    gene_list_str =""
+    
+    for i  in range (0, len(gene_list)-1):
+        gene = gene_list[i]
+        #print(" \\\""+ gene +"\\\",", end = ' ')
+        gene_list_str = gene_list_str + " \\\""+ gene +"\\\","
+        
+    gene = gene_list[i+1]
+    gene_list_str = gene_list_str + " \\\""+ gene +"\\\""
+    command_str = "too-many-cells make-tree --matrix-path /cluster/home/t116508uhn/64630/spaceranger_output_new/ --labels-file /cluster/home/t116508uhn/64630/IX_annotation_artifacts.csv --prior /cluster/home/t116508uhn/64630/TAGConv_test_r4_org/ --feature-column 2   --output /cluster/home/t116508uhn/64630/gene_expression_tree --draw-node-number --draw-leaf 'DrawItem (DrawContinuous ["+ gene_list_str +"])' --draw-scale-saturation 10  > /cluster/home/t116508uhn/64630/PCA_64embedding_pathologist_label_l1mp5_temp.csv"
+    output_log=os.system(command_str)    
+    
+    
 for signature in signature_info.keys():
     gene_list=signature_info[signature]
     print("\n"+signature+"\n")
