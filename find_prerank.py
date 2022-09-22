2526b4dc51840a9089cc8dabd8434815c4735be9
anne04 <anne.06.cse@gmail.com>
2022-09-22 11:23:21 -0400

    Update find_prerank.py

--- find_prerank.py
+++ find_prerank.py
@@ -46,7 +46,7 @@ def main(args):
     name_list = ['10_13_48_59_86', '10_13_48_86_59', '10_13_59_86_48', '13_48_59_86_10', '10_48_59_86_13']
     n_index = 0
     for n_index in range (0, len(name_list)):
-        toomany_label_file = '/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_59_86_org_whitelist.csv'
+        toomany_label_file = '/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_14_15_org_whitelist.csv'
         #toomany_label_file = '/cluster/home/t116508uhn/64630/differential_TAGConv_test_r4_'+name_list[n_index]+'_prerank.csv'
         #print(node_list[n_index])
         gene_dict=defaultdict(list)
@@ -54,7 +54,7 @@ def main(args):
             csv_file = csv.reader(file, delimiter=",")
             i = 0
             for line in csv_file:
-                if i == 0 or np.float64(line[1]) == np.inf or np.float64(line[1]) == -np.inf:
+                if len(line)<1 or i == 0 or np.float64(line[1]) == np.inf or np.float64(line[1]) == -np.inf:
                     i = i+1
                     continue
                 gene_dict[line[0]].append(np.float64(line[1]))
@@ -63,7 +63,7 @@ def main(args):
         for gene in gene_dict:
             gene_dict[gene]=np.mean(gene_dict[gene])
 
-        signature_file='/cluster/home/t116508uhn/64630/GeneList_KF_22Aug10.csv' # 1406
+        signature_file='/cluster/home/t116508uhn/64630/Geneset_22Sep21_Subtypesonly.csv' # 1406
         signature_info=defaultdict(list)
         #barcode_info.append("")
         with open(signature_file) as file:
@@ -79,23 +79,23 @@ def main(args):
         pre_res = gp.prerank(rnk = data_rnk,
                              gene_sets = signature_info,
                              threads=4,
-                             min_size=5,
-                             max_size=1000,
+                             min_size=0,
+                             max_size=5000,
                              permutation_num=1000, # reduce number to speed up testing
                              outdir=None, # don't write to disk
                              seed=6,
                              verbose=True, # see what's going on behind the scenes
                             )
         print(pre_res.res2d)
-        pre_res.res2d.to_csv('/cluster/home/t116508uhn/64630/'+'86_vs_59'+'_prerank_tagconv_test_r4.csv')
+        pre_res.res2d.to_csv('/cluster/home/t116508uhn/64630/'+'14_vs_15'+'_prerank_tagconv_test_r4.csv')
         #pre_res.res2d.to_csv('/cluster/home/t116508uhn/64630/'+str(node_list[n_index])+'_prerank_tagconv_test_r4.csv')
 
         terms = pre_res.res2d.Term
         for i in range (0, pre_res.res2d.shape[0]):
             # save figure
             # gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname=save_path+name_str+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
-            # gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname='/cluster/home/t116508uhn/64630/'+str(node_list[n_index])+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
-            gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname='/cluster/home/t116508uhn/64630/'+"86_vs_59"+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
+            #gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname='/cluster/home/t116508uhn/64630/'+str(node_list[n_index])+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
+             gseaplot(rank_metric=pre_res.ranking, term=terms[i], ofname='/cluster/home/t116508uhn/64630/'+"14_vs_15"+'_'+str(i)+'_prerank_tagconv_test_r4.svg', **pre_res.results[terms[i]])
 
 
       
