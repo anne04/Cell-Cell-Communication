b92de9b958b3057eb10b68bf1a276c2440bfc3e2
anne04 <anne.06.cse@gmail.com>
2023-02-03 16:29:23 -0500

    Update plot_altair_ccc_tissue.py

--- plot_altair_ccc_tissue.py
+++ plot_altair_ccc_tissue.py
@@ -10,8 +10,8 @@ import altairThemes # assuming you have altairThemes.py at your current directoy
 # enable the newly registered theme
 #alt.themes.enable("publishTheme")
 
-filepath = '/mnt/data0/fatema/public/ccc_plot/ccc_th95_tissue_plot.csv'
-savepath = '/mnt/data0/fatema/public/ccc_plot/'
+filepath = '/cluster/home/t116508uhn/64630/ccc_th95_tissue_plot_woBlankEdges.csv' #'/mnt/data0/fatema/public/ccc_plot/ccc_th95_tissue_plot.csv'
+savepath = '/cluster/home/t116508uhn/64630/' #'/mnt/data0/fatema/public/ccc_plot/'
 
 data_list_pd = pd.read_csv(filepath)
 set1 = altairThemes.get_colour_scheme("Set1", len(data_list_pd["component_label"].unique()))
@@ -24,4 +24,4 @@ chart = alt.Chart(data_list_pd).mark_point(filled=True, opacity = 1).encode(
     tooltip=['component_label']
 )#.configure_legend(labelFontSize=6, symbolLimit=50)
 
-chart.save(save_path+'ccc_th95_tissue_plot.html')
+chart.save(save_path+'ccc_th95_tissue_plot_woBlankEdges.html')
