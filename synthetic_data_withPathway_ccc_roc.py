44db968de2e161c9512f0aa55a7775c24f813cab
anne04 <anne.06.cse@gmail.com>
2023-04-03 16:27:41 -0400

    Update synthetic_data_withPathway_ccc_roc.py

--- synthetic_data_withPathway_ccc_roc.py
+++ synthetic_data_withPathway_ccc_roc.py
@@ -1436,9 +1436,10 @@ while percentage_value > 0:
 ###########################################   
 filename = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10"]
 total_runs = 10
-for run_time in range (0, total_runs):
+for run_time in range (1,2): #total_runs):
     run = run_time
-    X_attention_filename = args.embedding_data_path + args.data_name + '/' + 'synthetic_data_ccc_roc_control_model_4_path_threshold_distance_e_relu_3dim_'+filename[run]+'_attention_l1.npy' #withFeature_4_pattern_overlapped_highertail, tp7p_,4_pattern_differentLRs, tp7p_broad_active, 4_r3,5_close, overlap_noisy, 6_r3
+    X_attention_filename = args.embedding_data_path + args.data_name + '/' + 'synthetic_data_ccc_roc_control_model_4_path_threshold_distance_e_3dim_'+filename[run]+'_attention_l1.npy' #withFeature_4_pattern_overlapped_highertail, tp7p_,4_pattern_differentLRs, tp7p_broad_active, 4_r3,5_close, overlap_noisy, 6_r3
+    #X_attention_filename = args.embedding_data_path + args.data_name + '/' + 'synthetic_data_ccc_roc_control_model_4_path_threshold_distance_e_relu_3dim_'+filename[run]+'_attention_l1.npy' #withFeature_4_pattern_overlapped_highertail, tp7p_,4_pattern_differentLRs, tp7p_broad_active, 4_r3,5_close, overlap_noisy, 6_r3
     #X_attention_filename = args.embedding_data_path + args.data_name + '/' + 'synthetic_data_ccc_roc_control_model_4_path_threshold_distance_e_gatconv_3dim_'+filename[run]+'_attention_l1.npy' #withFeature_4_pattern_overlapped_highertail, tp7p_,4_pattern_differentLRs, tp7p_broad_active, 4_r3,5_close, overlap_noisy, 6_r3
     #X_attention_filename = args.embedding_data_path + args.data_name + '/' + 'synthetic_data_ccc_roc_control_model_4_path_threshold_distance_e_tanh_3dim_'+filename[run]+'_attention_l1.npy' #withFeature_4_pattern_overlapped_highertail, tp7p_,4_pattern_differentLRs, tp7p_broad_active, 4_r3,5_close, overlap_noisy, 6_r3
     X_attention_bundle = np.load(X_attention_filename, allow_pickle=True) 
@@ -1487,6 +1488,7 @@ for run_time in range (0, total_runs):
     
     percentage_value = 100
     while percentage_value > 0:
+        distribution_partial = []
         percentage_value = percentage_value - 10
     #for percentage_value in [79, 85, 90, 93, 95, 97]:
         existing_lig_rec_dict = []
@@ -1516,6 +1518,7 @@ for run_time in range (0, total_runs):
                         ccc_index_dict[j] = ''
                         existing_lig_rec_dict[i][j].append(lig_rec_dict[i][j][k])
                         count = count + 1
+                        distribution_partial.append(attention_scores[i][j][k])
 
         #############
         #positive_class = 0  
@@ -1545,7 +1548,14 @@ for run_time in range (0, total_runs):
         plot_dict['FPR'].append(FPR_value)
         plot_dict['TPR'].append(TPR_value)
         plot_dict['Type'].append('run_'+str(run+1))
-                     
+        
+        plt.hist(distribution_partial, color = 'blue', bins = int(len(distribution_partial)/5))
+        save_path = '/cluster/home/t116508uhn/64630/'
+        plt.savefig(save_path+'distribution_e_3d_relu_partial_'+filename[run]+'_'+str(percentage_value)+'.svg', dpi=400)
+        #plt.savefig(save_path+'distribution_e_3d_gatconv_'+filename[run]+'.svg', dpi=400)
+        #plt.savefig(save_path+'distribution_e_3d_tanh_'+filename[run]+'.svg', dpi=400)
+        plt.clf()
+
 
 data_list_pd = pd.DataFrame(plot_dict)    
 chart = alt.Chart(data_list_pd).mark_line().encode(
