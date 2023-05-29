19d646a2b7a26329a466c5f949c958098dadb23a
anne04 <anne.06.cse@gmail.com>
2023-05-29 17:02:44 -0400

    Update COMMOT_synthetic.py

--- COMMOT_synthetic.py
+++ COMMOT_synthetic.py
@@ -48,18 +48,73 @@ distance_measure = 'knn'  #'threshold_dist' # <-----------
 datatype = 'path_mixture_of_distribution' #'path_equally_spaced' #
 
 options = 'dt-path_mixture_of_distribution_lrc8_cp100_noise0_random_overlap_knn_cellCount2534_f_3dim'
+#options = 'dt-path_equally_spaced_lrc8_cp100_noise0_random_overlap_threshold_dist_cellCount3000_e_3dim'
 
+#gene_vs_cell = pd.read_csv('/cluster/home/t116508uhn/synthetic_type4_e_gene_vs_cell.csv', index_col=0)  
 gene_vs_cell = pd.read_csv('/cluster/home/t116508uhn/synthetic_gene_vs_cell_type6_f.csv', index_col=0)  
 cell_vs_gene = gene_vs_cell.transpose()
-adata_synthetic = anndata.AnnData(cell_vs_gene)
+
+
+#df_x=pd.read_csv('/cluster/home/t116508uhn/synthetic_cell_type4_e_x.csv',header=None)
+#df_y=pd.read_csv('/cluster/home/t116508uhn/synthetic_cell_type4_e_y.csv',header=None)
 
 df_x=pd.read_csv('/cluster/home/t116508uhn/synthetic_cell_type6_f_x.csv',header=None)
 df_y=pd.read_csv('/cluster/home/t116508uhn/synthetic_cell_type6_f_y.csv',header=None)
 
-gene_vs_cell = pd.read_csv('/cluster/home/t116508uhn/synthetic_gene_vs_cell_type6_f.csv', index_col=0)  
+coordinate_synthetic = np.zeros((cell_vs_gene.shape[0],2))
+for i in range (0, len(df_x)):
+    coordinate_synthetic[i][0] = df_x[0][i]
+    coordinate_synthetic[i][1] = df_y[0][i]
+
+spatial_dict = dict()
+spatial_dict['spatial'] = coordinate_synthetic
+adata_synthetic = anndata.AnnData(cell_vs_gene, obsm=spatial_dict)
+
+#lr_db = pd.read_csv("/cluster/home/t116508uhn/synthetic_lr_type4_e.csv")
+lr_db = pd.read_csv("/cluster/home/t116508uhn/synthetic_lr_type6_f.csv")
+pathways = ['1','2','3','4','5','6','7','8']
+types = ['secreted signaling', 'secreted signaling', 'secreted signaling', 'secreted signaling', 'secreted signaling', 'secreted signaling', 'secreted signaling', 'secreted signaling']
+lr_db['pathways'] = pathways
+lr_db['type'] = types
+
+ct.tl.spatial_communication(adata_synthetic, database_name='syndb', df_ligrec=lr_db, dis_thr=12, heteromeric=True, pathway_sum=True)
+adata_synthetic.write("/cluster/projects/schwartzgroup/fatema/syn_type6_f_commot_adata.h5ad")
+#ct.tl.spatial_communication(adata_synthetic, database_name='syndb', df_ligrec=lr_db, dis_thr=2, heteromeric=True, pathway_sum=True)
+#adata_synthetic.write("/cluster/projects/schwartzgroup/fatema/syn_type4_e_commot_adata.h5ad")
 
 
+LR_pairs = ['g0-g8', 'g1-g9', 'g2-g10', 'g3-g11', 'g4-g12', 'g5-g13', 'g6-g14', 'g7-g15']
 
+#################################################################################
+attention_scores = []
+lig_rec_dict = []
+datapoint_size = cell_vs_gene.shape[0]
+for i in range (0, datapoint_size):
+    attention_scores.append([])   
+    lig_rec_dict.append([])   
+    for j in range (0, datapoint_size):	
+        attention_scores[i].append([])   
+        attention_scores[i][j] = []
+        lig_rec_dict[i].append([])   
+        lig_rec_dict[i][j] = []
+
+distribution = []
+for pair_index in range(0, len(LR_pairs)):
+    pair = LR_pairs[pair_index]
+    key_pair = 'commot-syndb-' + pair
+    for i in range (0, datapoint_size):
+        for j in range (0, datapoint_size):
+            if adata_synthetic.obsp[key_pair][i,j]>0:
+                attention_scores[i][j].append(adata_synthetic.obsp[key_pair][i,j])
+                lig_rec_dict[i][j].append(pair_index)
+                distribution.append(adata_synthetic.obsp[key_pair][i,j])
+            
+            
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_type6_f_commot_result', 'wb') as fp:
+    pickle.dump([attention_scores, lig_rec_dict, distribution], fp)            
+
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_type4_e_commot_result', 'wb') as fp:
+    pickle.dump([attention_scores, lig_rec_dict, distribution], fp)            
     
     
 
