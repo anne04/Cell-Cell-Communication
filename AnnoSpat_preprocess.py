f5444f805c66aa36e347cfc27acdf587a4cb4c6e
anne04 <anne.06.cse@gmail.com>
2023-07-24 17:10:38 -0400

    Create AnnoSpat_preprocess.py

---
+++ AnnoSpat_preprocess.py
@@ -0,0 +1,48 @@
+import os
+#import glob
+import pandas as pd
+#import shutil
+import copy
+import csv
+import numpy as np
+import sys
+
+df = pd.read_csv('/mnt/data1/gw/research/sc_integration/data/imc/raw_data/mgDF.csv', sep=",", header=0,index_col=0) 
+
+# seperate the protein names, (x, y), and cell names
+
+cell_name = []
+x_coord = []
+y_coord = []
+for i in range (0, len(df.index)):
+    cell_name.append(df.index[i])
+    x_coord.append(df['CenterX'][df.index[i]])
+    y_coord.append(df['CenterY'][df.index[i]])
+    
+
+protein_name = []
+for i in range (2, len(df.columns)):
+    protein_name.append(df.columns[i])
+
+gene_vs_cell = np.zeros((len(protein_name),len(cell_name)))
+for i in range (0, len(protein_name)):
+    gene = protein_name[i]
+    for j in range (0, len(cell_name)):
+        cell = cell_name[j]
+        gene_vs_cell[i][j] = df[gene][cell]
+
+np.save("mnt/data1/fatema/gene_vs_cell_count_mgDF", gene_vs_cell)
+
+df = pd.DataFrame(protein_name)
+df.to_csv('mnt/data1/fatema/protein_marker_mgDF.csv', index=False, header=False)
+
+df = pd.DataFrame(cell_name)
+df.to_csv('mnt/data1/fatema/cell_id_mgDF.csv', index=False, header=False)
+
+df = pd.DataFrame(x_coord)
+df.to_csv('mnt/data1/fatema/x_coord_mgDF.csv', index=False, header=False)
+
+df = pd.DataFrame(y_coord)
+df.to_csv('mnt/data1/fatema/y_coord_mgDF.csv', index=False, header=False)
+
+   
