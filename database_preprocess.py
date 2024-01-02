99a8ac3fb6595d4b731b65992426b2a3962eb967
anne04 <anne.06.cse@gmail.com>
2024-01-02 16:58:02 -0500

    update

--- database_preprocess.py
+++ database_preprocess.py
@@ -6,11 +6,10 @@ import pandas as pd
 
 
 ################# for running Niches ###############################
-ligand_dict_dataset = defaultdict(list)
-cell_cell_contact = dict() 
 ligand_list = []
 receptor_list = []
-annotation_list = []   
+annotation_list = [] 
+reference_list = [] 
 
 cell_chat_file = '/cluster/home/t116508uhn/Human-2020-Jin-LR-pairs_cellchat.csv'
 df = pd.read_csv(cell_chat_file)
@@ -21,19 +20,13 @@ for i in range (0, df["ligand_symbol"].shape[0]):
         
     receptor_symbol_list = df["receptor_symbol"][i]
     receptor_symbol_list = receptor_symbol_list.split("&")
-    for receptor in receptor_symbol_list:
-        ligand_dict_dataset[ligand].append(receptor)
-        if df["annotation"][i] == 'Cell-Cell Contact':
-            cell_cell_contact[receptor] = '' 
-            annotation_list.append('Cell-Cell Contact')
-        else:
-            annotation_list.append('')
-            
+    for receptor in receptor_symbol_list:            
         ligand_list.append(ligand)
         receptor_list.append(receptor)
-        
+        annotation_list.append(df["annotation"][i])
+        reference_list.append(df['evidence'][i])
             
-print(len(ligand_dict_dataset.keys()))
+
 
 nichetalk_file = '/cluster/home/t116508uhn/NicheNet-LR-pairs.csv'   
 df = pd.read_csv(nichetalk_file)
@@ -42,13 +35,14 @@ for i in range (0, df["from"].shape[0]):
     receptor = df["to"][i]
     ligand_list.append(ligand)
     receptor_list.append(receptor)
-    annotation_list.append('')
+    annotation_list.append(' ')
+    reference_list.append(df['source'][i])
 
 # make a csv file with three columns: Ligand, Receptor, Annotation
 csv_record = []
-csv_record.append(['Ligand', 'Receptor', 'Annotation'])
+csv_record.append(['Ligand', 'Receptor', 'Annotation', 'Reference'])
 for i in range (0, len(ligand_list)):
-    csv_record.append([ligand_list[i], receptor_list[i], annotation_list[i]])
+    csv_record.append([ligand_list[i], receptor_list[i], annotation_list[i], reference_list[i]])
 
 df = pd.DataFrame(csv_record) # output 4
 df.to_csv('/cluster/home/t116508uhn/64630/NEST_database.csv', index=False, header=False)
