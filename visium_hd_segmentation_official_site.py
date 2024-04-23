ec13bbb3c94731c3471d1400897cad07bef4096b
anne04 <anne.06.cse@gmail.com>
2024-04-23 15:30:43 -0400

    Update visium_hd_segmentation_official_site.py

--- visium_hd_segmentation_official_site.py
+++ visium_hd_segmentation_official_site.py
@@ -203,7 +203,8 @@ img = normalize(img, min_percentile, max_percentile)
 # Predict cell nuclei using the normalized image
 # Adjust nms_thresh and prob_thresh as needed
 
-labels, polys = model.predict_instances_big(img, axes='YXC', block_size=4096, prob_thresh=0.8,nms_thresh=0.001, min_overlap=128, context=128, normalizer=None, n_tiles=(4,4,1))
+# prob_thresh=0.8, 0.75
+labels, polys = model.predict_instances_big(img, axes='YXC', block_size=4096, prob_thresh=0.7,nms_thresh=0.001, min_overlap=128, context=128, normalizer=None, n_tiles=(4,4,1))
 
 
 # Creating a list to store Polygon geometries
@@ -229,10 +230,14 @@ gdf['id'] = [f"ID_{i+1}" for i, _ in enumerate(gdf.index)]
 cmap=ListedColormap(['grey'])
 
 
-with gzip.open(dir_base+ 'gdf_p8', 'wb') as fp:  #b, a:[0:5]   
+with gzip.open(dir_base+ 'gdf_p7', 'wb') as fp:     
     pickle.dump(gdf, fp)
+
+#fp = gzip.open(dir_base+ 'gdf_p8', 'rb')   
+#gdf = pickle.load(fp)
+
 # Create Plot
-plot_mask_and_save_image(title="Region of Interest 1",gdf=gdf,bbox=(12844,7700,13760,8664),cmap=cmap,img=img,output_name=dir_base+"image_mask.ROI1.tif")
+plot_mask_and_save_image(title="Region of Interest 1",gdf=gdf,bbox=(12844+12844,7700,12844+13760,8664),cmap=cmap,img=img,output_name=dir_base+"image_mask.ROI1.tif")
 plot_mask_and_save_image(title="Region full",gdf=gdf,cmap=cmap,img=img,output_name=dir_base+"image_mask_full.tif")
 
 # Load Visium HD data
@@ -302,8 +307,35 @@ summed_counts = summed_counts.tocsr()
 grouped_filtered_adata = anndata.AnnData(X=summed_counts,obs=pd.DataFrame(polygon_id,columns=['id'],index=polygon_id),var=filtered_adata.var)
 
 %store grouped_filtered_adata
-grouped_filtered_adata.write_h5ad(filename='/cluster/projects/schwartzgroup/fatema/data/Visium_HD_Human_Colon_Cancer_square_002um_outputs/grouped_filtered_adata_p8.h5ad', compression='gzip')
-grouped_filtered_adata.write_h5(filename='/cluster/projects/schwartzgroup/fatema/data/Visium_HD_Human_Colon_Cancer_square_002um_outputs/grouped_filtered_adata_p8.h5', compression='gzip')
+grouped_filtered_adata.write_h5ad(filename='/cluster/projects/schwartzgroup/fatema/data/Visium_HD_Human_Colon_Cancer_square_002um_outputs/grouped_filtered_adata_p7.h5ad', compression='gzip')
+
+
+# Store the area of each nucleus in the GeoDataframe
+gdf['area'] = gdf['geometry'].area
+# Calculate quality control metrics for the original AnnData object
+sc.pp.calculate_qc_metrics(grouped_filtered_adata, inplace=True)
+
+# Plot the nuclei area distribution before and after filtering
+#plot_nuclei_area(gdf=gdf,area_cut_off=500)
+# Plot total UMI distribution
+#total_umi(grouped_filtered_adata, 100)
+
+# Create a mask based on the 'id' column for values present in 'gdf' with 'area' less than 500
+mask_area = grouped_filtered_adata.obs['id'].isin(gdf[gdf['area'] < 500].id)
+# Create a mask based on the 'total_counts' column for values greater than 100
+mask_count = grouped_filtered_adata.obs['total_counts'] > 100
+# Apply both masks to the original AnnData to create a new filtered AnnData object
+count_area_filtered_adata = grouped_filtered_adata[mask_area & mask_count, :]
+
+# Calculate quality control metrics for the filtered AnnData object
+sc.pp.calculate_qc_metrics(count_area_filtered_adata, inplace=True)
+
+count_area_filtered_adata.write_h5ad(filename='/cluster/projects/schwartzgroup/fatema/data/Visium_HD_Human_Colon_Cancer_square_002um_outputs/count_area_filtered_adata_p7.h5ad', compression='gzip')
+# Plot and save the clustering results
+# plot_clusters_and_save_image(title="Region of interest 1", gdf=gdf, img=img, adata=count_area_filtered_adata, bbox=(12844,7700,13760,8664), color_by_obs='clusters', output_name=dir_base+"image_clustering.ROI1.tiff")
+# Plot Lyz1 gene expression
+plot_gene_and_save_image(title="Region of interest 1", gdf=gdf, gene='MUC2', img=img, adata=count_area_filtered_adata, bbox=(12844+12844,7700,12844+13760,8664),output_name=dir_base+"image_Muc2.ROI1.tiff")
+plot_gene_and_save_image(title="Region full", gdf=gdf, gene='MUC2', img=img, adata=count_area_filtered_adata,output_name=dir_base+"image_full_Muc2.tiff")
 
 
 
