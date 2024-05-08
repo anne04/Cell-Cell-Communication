6d3b6a03934e60712652b9567c487972f528ad94
anne04 <anne.06.cse@gmail.com>
2024-05-08 17:44:23 -0400

    Update preprocess_cytosignal.py

--- preprocess_cytosignal.py
+++ preprocess_cytosignal.py
@@ -2,6 +2,7 @@ import gzip
 import pandas as pd
 import pickle
 from collections import defaultdict
+import numpy as np
 
 options_list = ['dt-path_equally_spaced_lrc1467_cp100_noise0_random_overlap_threshold_dist_cellCount3000_3dim_3patterns_temp',
                'dt-path_equally_spaced_lrc1467_cp100_noise30_lowNoise_random_overlap_threshold_dist_cellCount3000_3dim_3patterns_temp',
@@ -26,39 +27,43 @@ for op_index in range (0, len(options_list)):
     with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_ccc_roc_control_model_'+ options +'_'+'_cellvsgene_'+ 'not_quantileTransformed', 'rb') as fp:
     #with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_ccc_roc_control_model_'+ options +'_cellvsgene', 'rb') as fp: #'not_quantileTransformed'
         cell_vs_gene = pickle.load(fp)
+        
+    min_gene_expr = np.min(cell_vs_gene)
+    min_gene_expr = min_gene_expr*(-1)
+    # scale the gene values from 0 to 1    
+    data_list = defaultdict(list)
+    for i in range (0, cell_vs_gene.shape[0]):
+        for j in range (0, cell_vs_gene.shape[1]):
+            data_list['a'+str(i)].append(int(np.round(min_gene_expr+cell_vs_gene[i][j])))
+
     
+    data_list_pd = pd.DataFrame(data_list)        
+    gene_name = []
+    for i in range (0, cell_vs_gene.shape[1]):
+        gene_name.append('G'+str(i))
+        
+    data_list_pd[' ']=gene_name   
+    data_list_pd = data_list_pd.set_index(' ')    
+    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/synthetic_raw_gene_vs_cell_'+options+'.csv')
+
+    ########################################################################
     with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_ccc_roc_control_model_'+ options +'_xny', 'rb') as fp:
         temp_x, temp_y, ccc_region  = pickle.load(fp)
     
-    data_list = defaultdict(list)
-    min_value = np.min(cell_vs_gene)
-    max_value = np.max(cell_vs_gene)
-    for i in range (0, cell_vs_gene.shape[0]):
-        for j in range (0, cell_vs_gene.shape[1]):
-            data_list['a'+str(i)].append((cell_vs_gene[i][j]-min_value)/(max_value-min_value))
-            
     cell_id = list(data_list.keys())    
     spatial_dict = {' ': cell_id, 'x': list(temp_x), 'y': list(temp_y)} 
     spatial_dict = pd.DataFrame(spatial_dict)  
     spatial_dict = spatial_dict.set_index(' ')  
     spatial_dict.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/synthetic_cell_'+options+'_spatial.csv')
   
-    data_list_pd = pd.DataFrame(data_list)    
-    gene_name = []
-    for i in range (0, cell_vs_gene.shape[1]):
-        gene_name.append('g'+str(i))
-        
-    data_list_pd[' ']=gene_name   
-    data_list_pd = data_list_pd.set_index(' ')    
-    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/synthetic_raw_gene_vs_cell_'+options+'.csv')
-
+    ########################################################################
     fp = gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'Tclass_synthetic_data_ccc_roc_control_model_'+ options, 'rb')  # at least one of lig or rec has exp > respective knee point          
     lr_database, lig_rec_dict_TP, random_activation = pickle.load(fp)
 
     unique_gene_name = dict()
     for i in range (0, len(lr_database)):
-        unique_gene_name['g'+str(lr_database[i][0])] = ''
-        unique_gene_name['g'+str(lr_database[i][1])] = ''
+        unique_gene_name['G'+str(lr_database[i][0])] = ''
+        unique_gene_name['G'+str(lr_database[i][1])] = ''
 
 
     data_list=dict()
@@ -69,7 +74,8 @@ for op_index in range (0, len(options_list)):
         data_list['gene_name'].append(gene)
         data_list['uniprot'].append(gene)
         data_list['hgnc_symbol'].append(gene)
-            
+
+    data_list_pd = pd.DataFrame(data_list)   
     data_list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/gene_to_u_'+options+'.csv', index=False) 
 
 
@@ -90,10 +96,10 @@ for op_index in range (0, len(options_list)):
     
     for i in range (0, len(lr_database)):
         data_list['id_cp_interaction'].append(str(i)) # this is also used as index
-        data_list['partner_a'].append('g'+str(lr_database[i][0]))
-        data_list['partner_b'].append('g'+str(lr_database[i][1]))
-        data_list['protein_name_a'].append('g'+str(lr_database[i][0]))
-        data_list['protein_name_b'].append('g'+str(lr_database[i][1]))
+        data_list['partner_a'].append('G'+str(lr_database[i][0]))
+        data_list['partner_b'].append('G'+str(lr_database[i][1]))
+        data_list['protein_name_a'].append('G'+str(lr_database[i][0]))
+        data_list['protein_name_b'].append('G'+str(lr_database[i][1]))
         data_list['annotation_strategy'].append('curated')
         data_list['source'].append('synthetic')
         data_list['partner_a_is_recep'].append(False)
@@ -104,7 +110,7 @@ for op_index in range (0, len(options_list)):
     data_list_pd = pd.DataFrame(data_list)   
     data_list_pd[' '] = data_list['id_cp_interaction']
     data_list_pd = data_list_pd.set_index(' ')  
-    data_list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/inter.index_'+options+'.csv', index=False) 
+    data_list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/inter.index_'+options+'.csv') #, index=False 
 
     protein_name_ligand = []
     interaction_id_ligand = []
@@ -112,9 +118,9 @@ for op_index in range (0, len(options_list)):
     interaction_id_receptor = []
 
     for i in range (0, len(lr_database)):
-        protein_name_ligand.append('g'+str(lr_database[i][0]))
+        protein_name_ligand.append('G'+str(lr_database[i][0]))
         interaction_id_ligand.append(data_list['id_cp_interaction'][i])
-        protein_name_receptor.append('g'+str(lr_database[i][1]))
+        protein_name_receptor.append('G'+str(lr_database[i][1]))
         interaction_id_receptor.append(data_list['id_cp_interaction'][i])
 
     list_pd = pd.DataFrame(protein_name_ligand) 
