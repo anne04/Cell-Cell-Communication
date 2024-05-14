93ed2433b6318e1742e4fa49aa41fc56b07b7897
anne04 <anne.06.cse@gmail.com>
2024-05-14 10:51:01 -0400

    Update cytosignal_preprocess_human_lymph.py

--- cytosignal_preprocess_human_lymph.py
+++ cytosignal_preprocess_human_lymph.py
@@ -4,11 +4,11 @@ import pickle
 from collections import defaultdict
 import numpy as np
 import scanpy as sc
+from scipy import sparse
 
 
 
-
-adata_h5 = sc.read_visium(path=args.data_from, count_file='filtered_feature_bc_matrix.h5')
+adata_h5 = sc.read_visium(path='/cluster/projects/schwartzgroup/fatema/data/V1_Human_Lymph_Node_spatial/', count_file='filtered_feature_bc_matrix.h5')
 print('input data read done')
 gene_count_before = len(list(adata_h5.var_names) )    
 sc.pp.filter_genes(adata_h5, min_cells=1)
@@ -16,8 +16,9 @@ gene_count_after = len(list(adata_h5.var_names) )
 print('Gene filtering done. Number of genes reduced from %d to %d'%(gene_count_before, gene_count_after))
 gene_ids = list(adata_h5.var_names)
 coordinates = adata_h5.obsm['spatial']
-cell_vs_gene = adata_h5.X
 cell_barcode = np.array(adata_h5.obs.index)
+cell_vs_gene = sparse.csr_matrix.toarray(adata_h5.X)
+
 
 data_list = defaultdict(list)
 for i in range (0, cell_vs_gene.shape[0]):
@@ -35,7 +36,7 @@ data_list_pd = data_list_pd.set_index(' ')
 data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/human_lymph_raw_gene_vs_cell.csv')
 
 cell_id = list(data_list.keys())    
-spatial_dict = {' ': cell_id, 'x': list(coordinates[]), 'y': list(coordinates)} 
+spatial_dict = {' ': cell_id, 'x': list(coordinates[:,0]), 'y': list(coordinates[:,1])} 
 spatial_dict = pd.DataFrame(spatial_dict)  
 spatial_dict = spatial_dict.set_index(' ')  
 spatial_dict.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/human_lymph_spatial.csv')
