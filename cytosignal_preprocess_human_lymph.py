cfa3670c34f5e52f0b72c79eb52f28aca3a42762
anne04 <anne.06.cse@gmail.com>
2024-05-14 11:33:41 -0400

    Update cytosignal_preprocess_human_lymph.py

--- cytosignal_preprocess_human_lymph.py
+++ cytosignal_preprocess_human_lymph.py
@@ -10,6 +10,7 @@ from scipy import sparse
 
 adata_h5 = sc.read_visium(path='/cluster/projects/schwartzgroup/fatema/data/V1_Human_Lymph_Node_spatial/', count_file='filtered_feature_bc_matrix.h5')
 print('input data read done')
+adata_h5.var_names_make_unique()
 gene_count_before = len(list(adata_h5.var_names) )    
 sc.pp.filter_genes(adata_h5, min_cells=1)
 gene_count_after = len(list(adata_h5.var_names) )  
@@ -18,6 +19,9 @@ gene_ids = list(adata_h5.var_names)
 coordinates = adata_h5.obsm['spatial']
 cell_barcode = np.array(adata_h5.obs.index)
 cell_vs_gene = sparse.csr_matrix.toarray(adata_h5.X)
+
+for i in range (0, len(cell_barcode)):
+    cell_barcode[i] = cell_barcode[i].replace('-','.')
 
 
 data_list = defaultdict(list)
