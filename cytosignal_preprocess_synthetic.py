65f8123f1c160f8d13ef376696a62f5f8d66fdfb
anne04 <anne.06.cse@gmail.com>
2024-05-14 20:05:44 -0400

    Update cytosignal_preprocess_synthetic.py

--- cytosignal_preprocess_synthetic.py
+++ cytosignal_preprocess_synthetic.py
@@ -28,17 +28,23 @@ for op_index in range (0, len(options_list)):
     with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_ccc_roc_control_model_'+ options +'_cellvsgene', 'rb') as fp: #'not_quantileTransformed'
         cell_vs_gene = pickle.load(fp)
       
-    print(np.min(cell_vs_gene))
     cell_vs_gene = cell_vs_gene*100
+    print(np.min(cell_vs_gene))
     min_gene_expr = np.min(cell_vs_gene)
-    min_gene_expr = min_gene_expr*(-1)
+    toggle = 0
+    if min_gene_expr<0:
+        toggle = 1
+        min_gene_expr = min_gene_expr*(-1)
     # scale the gene values from 0 to 1    
     data_list = defaultdict(list)
     for i in range (0, cell_vs_gene.shape[0]):
         for j in range (0, cell_vs_gene.shape[1]):
-            #data_list['a'+str(i)].append(int(np.round(min_gene_expr+cell_vs_gene[i][j])))
+            if toggle == 1:
+                cell_vs_gene[i][j] = min_gene_expr+cell_vs_gene[i][j]
+            
             data_list['a'+str(i)].append(int(np.round(cell_vs_gene[i][j])))
-    
+          
+    print('min %d and max %d'%(np.min(cell_vs_gene), np.max(cell_vs_gene)))
     data_list_pd = pd.DataFrame(data_list)        
     gene_name = []
     for i in range (0, cell_vs_gene.shape[1]):
@@ -46,7 +52,7 @@ for op_index in range (0, len(options_list)):
         
     data_list_pd[' ']=gene_name   
     data_list_pd = data_list_pd.set_index(' ')    
-    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/synthetic_raw_gene_vs_cell_'+options+'.csv')
+    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/synthetic_raw_gene_vs_cell_'+options+'.csv')
 
     ########################################################################
     with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_ccc_roc_control_model_'+ options +'_xny', 'rb') as fp:
@@ -56,7 +62,7 @@ for op_index in range (0, len(options_list)):
     spatial_dict = {' ': cell_id, 'x': list(temp_x), 'y': list(temp_y)} 
     spatial_dict = pd.DataFrame(spatial_dict)  
     spatial_dict = spatial_dict.set_index(' ')  
-    spatial_dict.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/synthetic_cell_'+options+'_spatial.csv')
+    spatial_dict.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/synthetic_cell_'+options+'_spatial.csv')
   
     ########################################################################
     fp = gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'Tclass_synthetic_data_ccc_roc_control_model_'+ options, 'rb')  # at least one of lig or rec has exp > respective knee point          
@@ -78,7 +84,7 @@ for op_index in range (0, len(options_list)):
         data_list['hgnc_symbol'].append(gene)
 
     data_list_pd = pd.DataFrame(data_list)   
-    data_list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/gene_to_u_'+options+'.csv', index=False) 
+    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/gene_to_u_'+options+'.csv', index=False) 
 
 
     data_list=dict()
@@ -112,7 +118,7 @@ for op_index in range (0, len(options_list)):
     data_list_pd = pd.DataFrame(data_list)   
     data_list_pd[' '] = data_list['id_cp_interaction']
     data_list_pd = data_list_pd.set_index(' ')  
-    data_list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/inter.index_'+options+'.csv') #, index=False 
+    data_list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/inter.index_'+options+'.csv') #, index=False 
 
     protein_name_ligand = []
     interaction_id_ligand = []
@@ -126,14 +132,52 @@ for op_index in range (0, len(options_list)):
         interaction_id_receptor.append(data_list['id_cp_interaction'][i])
 
     list_pd = pd.DataFrame(protein_name_ligand) 
-    list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/protein_name_ligand_'+options+'.csv', header=False, index=False) 
+    list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/protein_name_ligand_'+options+'.csv', header=False, index=False) 
 
     list_pd = pd.DataFrame(interaction_id_ligand) 
-    list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/interaction_id_ligand_'+options+'.csv', header=False, index=False) 
+    list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/interaction_id_ligand_'+options+'.csv', header=False, index=False) 
 
     list_pd = pd.DataFrame(protein_name_receptor) 
-    list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/protein_name_receptor_'+options+'.csv', header=False, index=False) 
+    list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/protein_name_receptor_'+options+'.csv', header=False, index=False) 
 
     list_pd = pd.DataFrame(interaction_id_receptor) 
-    list_pd.to_csv('/cluster/home/t116508uhn/cytosignal/interaction_id_receptor_'+options+'.csv', header=False, index=False) 
+    list_pd.to_csv('/cluster/projects/schwartzgroup/fatema/find_ccc/cytosignal_metadata/interaction_id_receptor_'+options+'.csv', header=False, index=False) 
+'''
+0
+-1514.8639982391362
 
+min 0 and max 5966
+1
+-1588.0357282745742
+min 0 and max 6047
+2
+-1659.604418395068
+min 0 and max 6148
+3
+131.64238378638817
+min 131 and max 4319
+4
+104.6523318111215
+min 104 and max 4321
+5
+-31.63807988355923
+min 0 and max 4422
+6
+131.3582469862863
+min 131 and max 4262
+7
+77.6526719037674
+min 77 and max 4271
+8
+-32.888527193529086
+min 0 and max 4369
+9
+67.88465167205061
+min 67 and max 627
+10
+194.55597458895164
+min 194 and max 1747
+11
+116.20666755039852
+min 116 and max 947
+'''
