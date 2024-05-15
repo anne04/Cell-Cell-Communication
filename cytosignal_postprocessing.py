cc5e1be29bfdf20ee07631f3ecd2440d66cdd0d2
anne04 <anne.06.cse@gmail.com>
2024-05-15 12:20:12 -0400

    Update cytosignal_postprocessing.py

--- cytosignal_postprocessing.py
+++ cytosignal_postprocessing.py
@@ -23,6 +23,7 @@ options_list = ['dt-path_equally_spaced_lrc1467_cp100_noise0_random_overlap_thre
 
 for op_index in range (0, len(options_list)):
     print('%d'%op_index)
+  
     options = options_list[op_index]
 
     fp = gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'Tclass_synthetic_data_ccc_roc_control_model_'+ options, 'rb')  # at least one of lig or rec has exp > respective knee point          
@@ -86,67 +87,72 @@ for op_index in range (0, len(options_list)):
         lig_rec_dict[i][j].append(ccc_id)
         distribution.append(score)
 
-
+    '''
     plot_dict = defaultdict(list)
     percentage_value = 100
     while percentage_value > 0:
         percentage_value = percentage_value - 10
-        existing_lig_rec_dict = []
-        for i in range (0, datapoint_size):
-            existing_lig_rec_dict.append([])   
-            for j in range (0, datapoint_size):	
-                existing_lig_rec_dict[i].append([])   
-                existing_lig_rec_dict[i][j] = []
-    
-        ccc_index_dict = dict()
-        threshold_down =  np.percentile(sorted(distribution), percentage_value)
-        threshold_up =  np.percentile(sorted(distribution), 100)
-        connecting_edges = np.zeros((datapoint_size, datapoint_size))
-        rec_dict = defaultdict(dict)
-        total_edges_count = 0
-        for i in range (0, datapoint_size):
-            for j in range (0, datapoint_size):
-                #if i==j: 
-                #    continue
-                atn_score_list = attention_scores[i][j]
-                #print(len(atn_score_list))
-                
-                for k in range (0, len(atn_score_list)):
-                    if attention_scores[i][j][k] >= threshold_down and attention_scores[i][j][k] <= threshold_up: #np.percentile(sorted(distribution), 50):
-                        connecting_edges[i][j] = 1
-                        ccc_index_dict[i] = ''
-                        ccc_index_dict[j] = ''
-                        existing_lig_rec_dict[i][j].append(lig_rec_dict[i][j][k])
-                        total_edges_count = total_edges_count + 1
-                        
-    
-    
-        ############# 
-        print('total edges %d'%total_edges_count)
-        #negative_class = 0
-        confusion_matrix = np.zeros((2,2))
-        for i in range (0, datapoint_size):
-            for j in range (0, datapoint_size):
-    
-                #if i==j: 
-                #    continue
-                ''' 
-                if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i]:
-                    for k in range (0, len(lig_rec_dict_TP[i][j])):
-                        if lig_rec_dict_TP[i][j][k] in existing_lig_rec_dict[i][j]: #
-                            confusion_matrix[0][0] = confusion_matrix[0][0] + 1
-                        else:
-                            confusion_matrix[0][1] = confusion_matrix[0][1] + 1 
-    
-                '''
-                if len(existing_lig_rec_dict[i][j])>0:
-                    for k in existing_lig_rec_dict[i][j]:   
-                        if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i] and k in lig_rec_dict_TP[i][j]:
-                            #print("i=%d j=%d k=%d"%(i, j, k))
-                            confusion_matrix[0][0] = confusion_matrix[0][0] + 1
-                        else:
-                            confusion_matrix[1][0] = confusion_matrix[1][0] + 1                 
-                 
+    '''
+
+   ###########################  
+    percentage_value = 0
+    existing_lig_rec_dict = []
+    for i in range (0, datapoint_size):
+        existing_lig_rec_dict.append([])   
+        for j in range (0, datapoint_size):	
+            existing_lig_rec_dict[i].append([])   
+            existing_lig_rec_dict[i][j] = []
+
+    ccc_index_dict = dict()
+    threshold_down =  np.percentile(sorted(distribution), percentage_value)
+    threshold_up =  np.percentile(sorted(distribution), 100)
+    connecting_edges = np.zeros((datapoint_size, datapoint_size))
+    rec_dict = defaultdict(dict)
+    total_edges_count = 0
+    for i in range (0, datapoint_size):
+        for j in range (0, datapoint_size):
+            #if i==j: 
+            #    continue
+            atn_score_list = attention_scores[i][j]
+            #print(len(atn_score_list))
+            
+            for k in range (0, len(atn_score_list)):
+                if attention_scores[i][j][k] >= threshold_down and attention_scores[i][j][k] <= threshold_up: #np.percentile(sorted(distribution), 50):
+                    connecting_edges[i][j] = 1
+                    ccc_index_dict[i] = ''
+                    ccc_index_dict[j] = ''
+                    existing_lig_rec_dict[i][j].append(lig_rec_dict[i][j][k])
+                    total_edges_count = total_edges_count + 1
+                    
+
+
+    ############# 
+    print('total edges %d'%total_edges_count)
+    #negative_class = 0
+    confusion_matrix = np.zeros((2,2))
+    for i in range (0, datapoint_size):
+        for j in range (0, datapoint_size):
+
+            #if i==j: 
+            #    continue
+            ''' 
+            if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i]:
+                for k in range (0, len(lig_rec_dict_TP[i][j])):
+                    if lig_rec_dict_TP[i][j][k] in existing_lig_rec_dict[i][j]: #
+                        confusion_matrix[0][0] = confusion_matrix[0][0] + 1
+                    else:
+                        confusion_matrix[0][1] = confusion_matrix[0][1] + 1 
+
+            '''
+            if len(existing_lig_rec_dict[i][j])>0:
+                for k in existing_lig_rec_dict[i][j]:   
+                    if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i] and k in lig_rec_dict_TP[i][j]:
+                        #print("i=%d j=%d k=%d"%(i, j, k))
+                        confusion_matrix[0][0] = confusion_matrix[0][0] + 1
+                    else:
+                        confusion_matrix[1][0] = confusion_matrix[1][0] + 1                 
+
+##########################
         print('%d, %g, %g'%(percentage_value,  (confusion_matrix[1][0]/negative_class)*100, (confusion_matrix[0][0]/positive_class)*100))    
         FPR_value = (confusion_matrix[1][0]/negative_class)#*100
         TPR_value = (confusion_matrix[0][0]/positive_class)#*100
