ccba0ee84281dcc84663589f9980450d7ff18595
anne04 <anne.06.cse@gmail.com>
2024-09-05 11:54:29 -0400

    Update PDAC_COMMOT_relay.py

--- PDAC_COMMOT_relay.py
+++ PDAC_COMMOT_relay.py
@@ -85,13 +85,88 @@ for pair_index in range(0, len(LR_pair)):
                 distribution.append(adata.obsp[key_pair][i,j])
             
 
-with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + args.data_name + '_commot_result', 'wb') as fp:
-    pickle.dump([attention_scores, lig_rec_dict, distribution], fp)            
-
-#with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + args.data_name + '_classical_commot_result', 'wb') as fp:
+#with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + args.data_name + '_commot_result', 'wb') as fp:
 #    pickle.dump([attention_scores, lig_rec_dict, distribution], fp)            
 
+##################################################################################################################
+csv_record_final = []
+# columns are: from_cell, to_cell, ligand_gene, receptor_gene, rank, attention_score, component, from_id, to_id
 
+top20 = np.percentile(distribution, 80)
+top_hist = defaultdict(list)
+for i in range (0, datapoint_size):
+    for j in range (0, datapoint_size):
+        for k in range (0, len(attention_scores[i][j])):
+            score = attention_scores[i][j][k]
+            if score >= top20:
+                csv_record_final.append([cell_barcode[i], cell_barcode[j], ligand, receptor, -1, score, -1, i, j])
+                
+csv_record_final = sorted(csv_record_final, key = lambda x: x[5], reverse=True) # high to low
+
+####################### pattern finding ##########################################################################
+# make a dictionary to keep record of all the outgoing edges [to_node, ligand, receptor] for each node
+each_node_outgoing = defaultdict(list)
+for k in range (1, len(csv_record_final)-1): # last record is a dummy for histogram preparation
+    i = csv_record_final[k][6]
+    j = csv_record_final[k][7]
+    if i == j:
+        continue        
+    ligand = csv_record_final[k][2]
+    receptor = csv_record_final[k][3]
+    each_node_outgoing[i].append([j, ligand, receptor, k]) 
+
+# all possible 2-hop pattern count
+pattern_distribution = defaultdict(list)
+# pattern_distribution['ligand-receptor to ligand-receptor']=[1,1,1,1, ...]
+edge_list_2hop = []
+target_relay = 'CCL21-CXCR4 to CCL21-CXCR4'
+for i in each_node_outgoing:
+    for tupple in each_node_outgoing[i]: # first hop
+        j = tupple[0]
+        lig_rec_1 = tupple[1]+'-'+tupple[2]
+        record_id_1 = tupple[3]
+        if j in each_node_outgoing:
+            for tupple_next in each_node_outgoing[j]: # second hop
+                k = tupple_next[0]
+                if k == i or k == j:
+                    continue
+                lig_rec_2 = tupple_next[1]+'-'+tupple_next[2]
+                record_id_2 = tupple_next[3]
+                pattern_distribution[lig_rec_1 + ' to ' + lig_rec_2].append(1)
+                relay = lig_rec_1 + ' to ' + lig_rec_2
+                if relay == target_relay:
+                    edge_list_2hop.append([record_id_1,record_id_2])
+
+
+
+two_hop_pattern_distribution = []
+same_count = 0
+for key in pattern_distribution:
+    count = len(pattern_distribution[key])
+    two_hop_pattern_distribution.append([key, count]) 
+    #if lig_rec_1 == lig_rec_2:
+    #    same_count = same_count + 1
+
+two_hop_pattern_distribution = sorted(two_hop_pattern_distribution, key = lambda x: x[1], reverse=True) # high to low
+
+data_list=dict()
+data_list['X']=[]
+data_list['Y']=[] 
+for i in range (0, 100): #len(two_hop_pattern_distribution)):
+    data_list['X'].append(two_hop_pattern_distribution[i][0])
+    data_list['Y'].append(two_hop_pattern_distribution[i][1])
+    
+data_list_pd = pd.DataFrame({
+    'Relay Patterns': data_list['X'],
+    'Pattern Abundance (#)': data_list['Y']
+})
+
+chart = alt.Chart(data_list_pd).mark_bar().encode(
+    x=alt.X("Relay Patterns:N", axis=alt.Axis(labelAngle=45), sort='-y'),
+    y='Pattern Abundance (#)'
+)
+
+chart.save(output_name + args.data_name +'_pattern_distribution.html')
 
 top20 = np.percentile(distribution, 80)
 top_hist = defaultdict(list)
@@ -132,28 +207,6 @@ chart = alt.Chart(data_list_pd).mark_bar().encode(
 )
 
 chart.save('/cluster/home/t116508uhn/' + args.data_name +'_commot_classical_ccc.html')
-
-ct.tl.communication_direction(adata, database_name='cellchat',  lr_pair=['MDK','ITGA6_ITGB1'],  k=5) #pathway_name='CCL',
-print('Plot the CCC')
-ct.pl.plot_cell_communication(adata, database_name='cellchat', lr_pair=['MDK','ITGA6_ITGB1'], plot_method='grid', background_legend=True,
-    scale=0.00003, ndsize=8, grid_density=0.4, summary='sender', background='image', clustering='leiden', cmap='Alphabet',
-    normalize_v = True, normalize_v_quantile=0.995, filename='/cluster/home/t116508uhn/' + args.data_name + '_MDK-ITGA6_ITGB1_' + 'PSAP_cluster.pdf')
-
-ct.tl.communication_direction(adata, database_name='cellchat',  lr_pair=['MDK','SDC4'],  k=5) #pathway_name='CCL',
-print('Plot the CCC')
-ct.pl.plot_cell_communication(adata, database_name='cellchat', lr_pair=['MDK','SDC4'], plot_method='grid', background_legend=True,
-    scale=0.00003, ndsize=8, grid_density=0.4, summary='sender', background='image', clustering='leiden', cmap='Alphabet',
-    normalize_v = True, normalize_v_quantile=0.995, filename='/cluster/home/t116508uhn/' + args.data_name + '_MDK-SDC4_' + 'PSAP_cluster.pdf')
-
-ct.tl.communication_direction(adata, database_name='cellchat',  lr_pair=['MDK','NCL'],  k=5) #pathway_name='CCL',
-print('Plot the CCC')
-ct.pl.plot_cell_communication(adata, database_name='cellchat', lr_pair=['MDK','NCL'], plot_method='grid', background_legend=True,
-    scale=0.00003, ndsize=8, grid_density=0.4, summary='sender', background='image', clustering='leiden', cmap='Alphabet',
-    normalize_v = True, normalize_v_quantile=0.995, filename='/cluster/home/t116508uhn/' + args.data_name + '_MDK-NCL_' + 'PSAP_cluster.pdf')
-
-ct.pl.plot_cell_communication(adata, database_name='cellchat', lr_pair=['MDK','NCL'], plot_method='grid', background_legend=True,
-    scale=0.00003, ndsize=8, grid_density=0.4, summary='sender', background='image', clustering='leiden', cmap='Alphabet',
-    normalize_v = True, normalize_v_quantile=0.5, filename='/cluster/home/t116508uhn/' + args.data_name + '_MDK-NCL_p5' + 'PSAP_cluster.pdf')
 
 
 #############################################################
