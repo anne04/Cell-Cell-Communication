914d82ae6e76eee2f8bb373823ae745b48629bd1
anne04 <anne.06.cse@gmail.com>
2024-09-13 15:17:47 -0400

    Update pathway_search.py

--- pathway_search.py
+++ pathway_search.py
@@ -3,11 +3,12 @@ import csv
 import pickle
 from scipy import sparse
 import pandas as pd
-
+from collections import defaultdict
 species = 'Human'
 receptor = ''
+max_hop = 3
 
-def get_KG(){
+def get_KG(receptor_name, pathways_dict, max_hop){
 
 
 }
@@ -16,10 +17,13 @@ def get_KG(){
 
 
 pathways = pd.read_csv("pathways.csv")
-df.drop(columns=['B', 'C'])
-pathways.drop(columns=[pathways.columns[0], pathways.columns[3],pathways.columns[4],pathways.columns[5]])
+pathways = pathways.drop(columns=[pathways.columns[0], pathways.columns[3],pathways.columns[4],pathways.columns[5]])
 # keep only target species
 for i in range (0, len(pathways)):
-    if (pathways['species'][i]!=species):
+    if (pathways['species'][i]==species):
         pathways.drop([i])
 
+pathways_dict = defaultdict(list)
+for i in range (0, len(pathways)):
+    if (pathways['species'][i]==species):
+        pathways_dict[pathways['src'][i]].append([pathways['dest'][i], pathways['src_tf'][i], pathways['dest_tf'][i]])
