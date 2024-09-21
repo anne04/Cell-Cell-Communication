3d4aa6bd58012d22e917cb45faa3d9207442541a
anne04 <anne.06.cse@gmail.com>
2024-09-20 21:45:49 -0400

    Create roc_curve_plot.py

---
+++ roc_curve_plot.py
@@ -0,0 +1,166 @@
+
+import os
+#import glob
+import pandas as pd
+#import shutil 
+import copy
+import csv
+import numpy as np
+import sys
+from collections import defaultdict
+import stlearn as st
+import scanpy as sc
+import qnorm
+import scipy
+import pickle
+import gzip
+import matplotlib.pyplot as plt
+from scipy import sparse 
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import connected_components
+from sklearn.metrics.pairwise import euclidean_distances
+from kneed import KneeLocator
+
+import altairThemes
+import altair as alt
+
+alt.themes.register("publishTheme", altairThemes.publishTheme)
+# enable the newly registered theme
+alt.themes.enable("publishTheme")
+
+
+
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'Tclass_synthetic_data_ccc_roc_control_model_'+ options , 'rb') as fp:  # +'_'+'notQuantileTransformed'at least one of lig or rec has exp > respective knee point          
+    lr_database, lig_rec_dict_TP, random_activation = pickle.load( fp)
+
+
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'adjacency_records_synthetic_data_ccc_roc_control_model_'+ options , 'rb') as fp:  # +'_'+'notQuantileTransformed'at least one of lig or rec has exp > respective knee point          
+    row_col, edge_weight, lig_rec  = pickle.load(fp)  #, lr_database, lig_rec_dict_TP, random_activation
+    
+
+	
+max_tp_distance = 0
+datapoint_size = temp_x.shape[0]              
+total_type = np.zeros((len(lr_database)))
+for i in range (0, datapoint_size):
+    for j in range (0, datapoint_size):
+        if i==j: 
+            continue
+        if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i] and len(lig_rec_dict_TP[i][j]) > 0:
+            for k in range (0, len(lig_rec_dict_TP[i][j])):
+                total_type[lig_rec_dict_TP[i][j][k]] = total_type[lig_rec_dict_TP[i][j][k]] + 1
+                if max_tp_distance<distance_matrix[i,j]:
+                    max_tp_distance = distance_matrix[i,j]
+count = 0
+for index in range (0, len(row_col)):
+    i = row_col[index][0]
+    j = row_col[index][1]
+    if i!=j:
+	    count = count +1     
+		
+positive_class = np.sum(total_type)
+negative_class = count - positive_class           
+
+
+
+
+
+
+
+######################### COMMOT ###############################################################################################################
+
+
+
+attention_scores = []
+lig_rec_dict = []
+datapoint_size = temp_x.shape[0]
+for i in range (0, datapoint_size):
+    attention_scores.append([])   
+    lig_rec_dict.append([])   
+    for j in range (0, datapoint_size):	
+        attention_scores[i].append([])   
+        attention_scores[i][j] = []
+        lig_rec_dict[i].append([])   
+        lig_rec_dict[i][j] = []
+        
+distribution = []
+
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + 'synthetic_data_'+options+'_commot_result', 'rb') as fp:
+    attention_scores, lig_rec_dict, distribution = pickle.load(fp)            
+
+
+distribution = sorted(distribution, reverse=True)
+#distribution = distribution[0:len(row_col)] # len(distribution) = 6634880, len(row_col)=21659
+#negative_class=len(distribution)-confusion_matrix[0][0]
+
+plot_dict = defaultdict(list)
+percentage_value = 100
+while percentage_value > 0:
+    percentage_value = percentage_value - 10
+#for percentage_value in [79, 85, 90, 93, 95, 97]:
+    existing_lig_rec_dict = []
+    datapoint_size = temp_x.shape[0]
+    for i in range (0, datapoint_size):
+        existing_lig_rec_dict.append([])   
+        for j in range (0, datapoint_size):	
+            existing_lig_rec_dict[i].append([])   
+            existing_lig_rec_dict[i][j] = []
+
+    ccc_index_dict = dict()
+    threshold_down =  np.percentile(distribution, percentage_value)
+    threshold_up =  np.percentile(distribution, 100)
+    connecting_edges = np.zeros((temp_x.shape[0],temp_x.shape[0]))
+    rec_dict = defaultdict(dict)
+    total_edges_count = 0
+    for i in range (0, datapoint_size):
+        for j in range (0, datapoint_size):
+            atn_score_list = attention_scores[i][j]
+            
+            
+            for k in range (0, len(atn_score_list)):
+                if attention_scores[i][j][k] >= threshold_down and attention_scores[i][j][k] <= threshold_up: #np.percentile(sorted(distribution), 50):
+                    connecting_edges[i][j] = 1
+                    ccc_index_dict[i] = ''
+                    ccc_index_dict[j] = ''
+                    existing_lig_rec_dict[i][j].append(lig_rec_dict[i][j][k])
+                    total_edges_count = total_edges_count + 1
+                    
+
+
+    ############# 
+    print('total edges %d'%total_edges_count)
+    #negative_class = 0
+    confusion_matrix = np.zeros((2,2))
+    for i in range (0, datapoint_size):
+        for j in range (0, datapoint_size):
+
+            #if i==j: 
+            #    continue
+            ''' 
+            if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i]:
+                for k in range (0, len(lig_rec_dict_TP[i][j])):
+                    if lig_rec_dict_TP[i][j][k] in existing_lig_rec_dict[i][j]: #
+                        confusion_matrix[0][0] = confusion_matrix[0][0] + 1
+                    else:
+                        confusion_matrix[0][1] = confusion_matrix[0][1] + 1 
+
+            '''
+            if len(existing_lig_rec_dict[i][j])>0:
+                for k in existing_lig_rec_dict[i][j]:   
+                    if i in lig_rec_dict_TP and j in lig_rec_dict_TP[i] and k in lig_rec_dict_TP[i][j]:
+                        #print("i=%d j=%d k=%d"%(i, j, k))
+                        confusion_matrix[0][0] = confusion_matrix[0][0] + 1
+                    else:
+                        confusion_matrix[1][0] = confusion_matrix[1][0] + 1                 
+             
+    print('%d, %g, %g'%(percentage_value,  (confusion_matrix[1][0]/negative_class)*100, (confusion_matrix[0][0]/positive_class)*100))    
+    FPR_value = (confusion_matrix[1][0]/negative_class)#*100
+    TPR_value = (confusion_matrix[0][0]/positive_class)#*100
+    plot_dict['FPR'].append(FPR_value)
+    plot_dict['TPR'].append(TPR_value)
+    plot_dict['Type'].append('COMMOT') #_lowNoise
+
+
+with gzip.open("/cluster/projects/schwartzgroup/fatema/find_ccc/" + options +'_'+'COMMOT', 'wb') as fp: #b, b_1, a  11to20runs
+    pickle.dump(plot_dict_temp, fp) #a - [0:5]
+
