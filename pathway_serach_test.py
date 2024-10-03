634b6293e6938df1779f4e7e0fc01669762f7f49
anne04 <anne.06.cse@gmail.com>
2024-10-03 13:28:41 -0400

    Update pathway_serach_test.py

--- pathway_serach_test.py
+++ pathway_serach_test.py
@@ -8,30 +8,35 @@ from collections import deque
 
 
 species = 'Human'
-receptor = ''
+receptor = '' 
 max_hop = 3
 def pathway_expression(receptor, get_rows, gene_exist_list):
     table_info = filter_pathway(get_rows, gene_exist_list)
     adjacency_list = get_adjacency_list(table_info)
     TF_scores = get_bfs(adjacency_list, receptor)
     # take the weighted average of the TF
+    score = 0
+    for TF_gene in TF_scores:
+        hop = TF_scores[TF_gene] 
+        score = score + gene_exist_list[TF_gene]*(1/hop)
+        
     return score
 
 def get_bfs(adjacency_list, receptor):
-    TF_scores = dict()
+    TF_scores = defaultdict(int)
     q = deque()
     total_TF = len(adjacency_list.keys())-1
 
     hop_count = 1
     for i in range (0, len(adjacency_list[receptor])):
-        dest = adjacency_list[receptor][i]
+        dest = adjacency_list[receptor][i][0]
         q.append(dest)
         TF_scores[dest]=hop_count
         
-    while( len(TF_scores.keys())!= total_TF):
+    while(len(TF_scores.keys())!= total_TF):
         source_gene = q.popleft()
         for i in range (0, len(adjacency_list[source_gene])):
-            dest = adjacency_list[source_gene][i]
+            dest = adjacency_list[source_gene][i][0]
             if dest in TF_scores:
                 continue
                 
@@ -64,14 +69,21 @@ def filter_pathway(table_info, gene_exist_list): # gene_exist_list is a dictiona
 
 def get_adjacency_list(table_info):
     adjacency_list = defaultdict(list)
-       
-    for i in range (0 , len(table_info)):
+    unique_gene_set = dict()   
+    for i in range (0, len(table_info)):
         source = table_info[i][0]
-        dest = table_info[i][1]
+        dest = table_info[i][1][0]
         adjacency_list[source].append(dest)
-
+        unique_gene_set[source]=''
+        unique_gene_set[dest]=''
+    
+    for gene in unique_gene_set:
+        if gene not in adjacency_list:
+            adjacency_list[gene]=[]
+            
     return adjacency_list
 
+'''
 pathways = pd.read_csv("pathways.csv")
 drop_row = []
 for i in range (0, len(pathways)):
@@ -79,10 +91,12 @@ for i in range (0, len(pathways)):
         drop_row.append(i)
 
 pathways = pathways.drop(drop_row)
-#pathways.to_csv("pathways_NEST.csv") 
-
-        
-pathways = pathways.drop(columns=[pathways.columns[0], pathways.columns[3],pathways.columns[4],pathways.columns[5]])
+pathways = pathways.drop(columns=[pathways.columns[0]])
+#pathways.to_csv("pathways_NEST.csv", index=False) 
+'''
+pathways = pd.read_csv("pathways_NEST.csv")        
+pathways = pathways.drop(columns=[pathways.columns[2],pathways.columns[3],pathways.columns[4]])
+pathways = pathways.drop_duplicates(ignore_index=True)
 # keep only target species
 pathways_dict = defaultdict(list)
 for i in range (0, len(pathways)):
@@ -97,5 +111,6 @@ get_KG('ERF', pathways_dict, 2, get_rows, current_hop=0) # save it
 
 # for each cell, for each receptor
 gene_exist_list = dict()
+gene_exist_list[]=
 # assign genes to gene exist list
 receptor_expression = pathway_expression(get_rows, gene_exist_list) 
