5fae214d95e5295853a6fba7bb11d05489313c07
anne04 <anne.06.cse@gmail.com>
2024-11-22 07:23:47 -0500

    Update data_preprocess_intra_NEST.py

--- data_preprocess_intra_NEST.py
+++ data_preprocess_intra_NEST.py
@@ -18,24 +18,6 @@ print('user input reading')
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     ################## Mandatory ####################################################################
-    '''
-    parser.add_argument( '--data_name', type=str, help='Name of the dataset', required=True)  
-    parser.add_argument( '--data_from', type=str, required=True, help='Path to the dataset to read from. Space Ranger outs/ folder is preferred. Otherwise, provide the *.mtx file of the gene expression matrix.')
-    ################# default is set ################################################################
-    parser.add_argument( '--data_to', type=str, default='input_graph/', help='Path to save the input graph (to be passed to GAT)')
-    parser.add_argument( '--metadata_to', type=str, default='metadata/', help='Path to save the metadata')
-    parser.add_argument( '--filter_min_cell', type=int, default=1 , help='Minimum number of cells for gene filtering') 
-    parser.add_argument( '--threshold_gene_exp', type=float, default=98, help='Threshold percentile for gene expression. Genes above this percentile are considered active.')
-    parser.add_argument( '--tissue_position_file', type=str, default='None', help='If your --data_from argument points to a *.mtx file instead of Space Ranger, then please provide the path to tissue position file.')
-    parser.add_argument( '--spot_diameter', type=float, default=89.43, help='Spot/cell diameter for filtering ligand-receptor pairs based on cell-cell contact information. Should be provided in the same unit as spatia data (for Visium, that is pixel).')
-    parser.add_argument( '--split', type=int, default=0 , help='How many split sections?') 
-    parser.add_argument( '--neighborhood_threshold', type=float, default=0 , help='Set neighborhood threshold distance in terms of same unit as spot diameter') 
-    parser.add_argument( '--database_path', type=str, default='database/NEST_database.csv' , help='Provide your desired ligand-receptor database path here. Default database is a combination of CellChat and NicheNet database.') 
-    parser.add_argument( '--add_intra', type=int, default=-1 , help='Set it to 1 for intracellular signaling pathway')
-    parser.add_argument( '--num_hops', type=int, default=3 , help='Maximum number of hops for intra signaling pathway')
-    parser.add_argument( '--species', type=str, default='Human', help='Species of the input sample')
-    args = parser.parse_args()  
-    '''
 
     parser = argparse.ArgumentParser()
     parser.add_argument( '--data_name', type=str, default='V1_Human_Lymph_Node_spatial_intra', help='Name of the dataset')  
