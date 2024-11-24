c427f17cf34ea12bf0cf31a172f125e57097d337
anne04 <anne.06.cse@gmail.com>
2024-11-24 02:37:56 -0500

    Update p_value_generation_NEST.py

--- p_value_generation_NEST.py
+++ p_value_generation_NEST.py
@@ -58,10 +58,8 @@ if __name__ == "__main__":
     if not os.path.exists(args.output_path):
         os.makedirs(args.output_path)
 
-##################### get metadata: barcode_info ################################## 
+##################### get metadata: input_graph ################################## 
 
-    with gzip.open(args.metadata_from +args.data_name+'_barcode_info', 'rb') as fp:  #b, a:[0:5]   _filtered
-        barcode_info = pickle.load(fp)
     
     with gzip.open(args.data_from + args.data_name + '_adjacency_records', 'rb') as fp:  #b, a:[0:5]  _filtered 
         row_col, edge_weight, lig_rec, total_num_cell = pickle.load(fp)
@@ -97,9 +95,43 @@ if __name__ == "__main__":
                 cell_cell_lr_score[i][j] = dict()
                 cell_cell_lr_score[i][j][lr_pair_id] = csv_record[record][8]
         else:
+            cell_cell_lr_score[i][j] = dict()
+            cell_cell_lr_score[i][j][lr_pair_id] = csv_record[record][8]
             
-                
+    ################## N time ##########################################
+    cell_cell_lr_score_shuffled = defaultdict(dict)
+    for shuffle_time in range (0, args.N):
+        ## permutate edge feature vector
+        random.shuffle(edge_weight)
+        # reassign the first 2 dimensions = distance and coexpression. Not changing the 3rd dimension (lr pair) because we may not get any value
+        # if that is not found between these two cells during edge shuffling
+        # save edge_weight as a temp
+         with gzip.open(args.data_from + args.data_name + '_adjacency_records_shuffled', 'wb') as fp:  #b, a:[0:5]  _filtered 
+            pickle.dump([row_col, edge_weight, lig_rec, total_num_cell],fp)
+        # run the model 3 times
+       
+        # postprocess results
 
+        # read it and get the values
+        inFile = args.output_path + args.model_name+'_top' + str(args.top_percent) + 'percent_temporary.csv'
+        df = pd.read_csv(inFile, sep=",")    
+        csv_record = df
+        for record in range (1, len(csv_record)-1):
+            i = csv_record[record][6]
+            j = csv_record[record][7]
+            ligand_gene = csv_record[record][2]
+            receptor_gene = csv_record[record][3]
+            lr_pair_id = lig_rec_db[ligand_gene][receptor_gene]
+            if i in cell_cell_lr_score_shuffled:
+                if j in cell_cell_lr_score_shuffled[i]: 
+                    cell_cell_lr_score_shuffled[i][j][lr_pair_id] = csv_record[record][8]
+                else:
+                    cell_cell_lr_score_shuffled[i][j] = dict()
+                    cell_cell_lr_score_shuffled[i][j][lr_pair_id] = csv_record[record][8]
+            else:
+                cell_cell_lr_score_shuffled[i][j] = dict()
+                cell_cell_lr_score_shuffled[i][j][lr_pair_id] = csv_record[record][8]
+           
     
     ####################################################################
     # ligand - receptor database 
