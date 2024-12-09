774e8295362321ed0e13f004fa598ce699b3f87a
anne04 <anne.06.cse@gmail.com>
2024-12-09 18:14:22 -0500

    Update confidence_interval_NEST.py

--- confidence_interval_NEST.py
+++ confidence_interval_NEST.py
@@ -3,23 +3,14 @@ import numpy as np
 import csv
 import pickle
 import statistics
-from scipy import sparse
-import scipy.io as sio
-import scanpy as sc 
 import matplotlib
-matplotlib.use('Agg')
-#matplotlib.use('TkAgg') 
 import matplotlib.pyplot as plt
 import numpy as np
-#from matplotlib.colors import LinearSegmentedColormap, to_hex, rgb2hex
-#from typing import List
-import qnorm
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import connected_components
 from scipy.stats import median_abs_deviation
 from scipy.stats import skew
 from collections import defaultdict
 import pandas as pd
+from random import choices
 import gzip
 #from kneed import KneeLocator
 import copy 
@@ -46,10 +37,23 @@ if __name__ == "__main__":
     ccc_list_filename = args.ccc_list_path + args.model_name+'_allCCC.csv'
 ##################### get metadata: barcode_info ###################################
     df = pd.read_csv(ccc_list_filename, sep=",")
-    csv_record = [list(df.columns)] + df.values.tolist()
+    csv_record = df.values.tolist()
     # from_cell, to_cell, ligand_gene, receptor_gene, rank, component, from_id, to_id,  attention_score
-     
+    total_edge = len(csv_record)
+    edge_index = []
+    for i in range (0, len(csv_record)):
+        edge_index.append(i)
 
+    new_sets = []
+    for i in range (0, num_new_set):
+        temp_set = choices(edge_index, k=(2*total_edge)/3)
+        new_sets.append(new_sets)
+
+    # replace new_sets[i] with attention scores
+    for i in range (0, num_new_set):
+        for j in range(0, len(new_sets[i])):
+            
+    
 
     df = pd.DataFrame(csv_record_final) # output 4
     df.to_csv(args.output_path + args.model_name+'_CCC_list_confident.csv', index=False, header=False)
