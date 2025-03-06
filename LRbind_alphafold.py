5869f07241f5f0489c2a8562d1f66db7821c0bae
anne04 <anne.06.cse@gmail.com>
2025-03-05 23:03:58 -0500

    Update LRbind_alphafold.py

--- LRbind_alphafold.py
+++ LRbind_alphafold.py
@@ -11,8 +11,8 @@ import gzip
 import math
 import argparse
 from random import randint 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     # ================ Specify data type firstly ===============
@@ -34,13 +34,20 @@ if __name__ == "__main__":
                 ['HLA-DRA', 'PTPRC'], ['PTPRC','CD74'], ['APOE', 'CXCR4'], 
                 ['HSP90B1', 'CXCR4'], ['HLA-F', 'CXCR4'], ['CCL19', 'CXCR4'], 
                 ['HLA-DRA', 'CD74']]
+
     
+    lrp_list =[['CCL21', 'CCR7'], ['RPS19','C5AR1'], ['HLA-A', 'CD8A'],
+               ['HLA-B','CCR7'], ['CCL21','CD8A'], ['RPS19','CCR7'],
+               ['HLA-DRA', 'CD4'], ['HLA-DRA','CCR7'],['CCL21', 'RPSA'],
+               ['HLA-B', 'C5AR1']
+        
+    ]
     for pair in lrp_list:
         ligand = pair[0]
         receptor = pair[1]
         lig_seq = dict_gene_seq[ligand]
         rec_seq = dict_gene_seq[receptor]
-        f = open(ligand+'_'+receptor+".fasta", "w")
+        f = open('dgi_'+ligand+'_'+receptor+".fasta", "w")
         f.write(">")
         f.write(ligand)
         f.write("\n")
