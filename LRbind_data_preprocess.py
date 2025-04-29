023a306067f3b8e34a1d86670425ea44530ac5b5
anne04 <anne.06.cse@gmail.com>
2025-04-29 16:37:16 -0400

    Update LRbind_data_preprocess.py

--- LRbind_data_preprocess.py
+++ LRbind_data_preprocess.py
@@ -33,16 +33,21 @@ if __name__ == "__main__":
     parser.add_argument( '--spot_diameter', type=float, default=89.43, help='Spot/cell diameter for filtering ligand-receptor pairs based on cell-cell contact information. Should be provided in the same unit as spatia data (for Visium, that is pixel).')
     parser.add_argument( '--split', type=int, default=0 , help='How many split sections?') 
     parser.add_argument( '--neighborhood_threshold', type=float, default=0 , help='Set neighborhood threshold distance in terms of same unit as spot diameter') 
+    parser.add_argument( '--num_hops', type=int, default=4 , help='Number of hops for direct connection')
     parser.add_argument( '--database_path', type=str, default='database/NEST_database.csv' , help='Provide your desired ligand-receptor database path here. Default database is a combination of CellChat and NicheNet database.') 
-    parser.add_argument( '--remove_LR', type=str, default=[['CCL19', 'CCR7']], help='Test LR to predict') #, required=True) # FN1-RPSA
+    parser.add_argument( '--remove_LR', type=str, help='Test LR to predict')
+    #parser.add_argument( '--remove_LR', type=str, default=[['CCL19', 'CCR7']], help='Test LR to predict') #, required=True) # FN1-RPSA
     parser.add_argument( '--target_lig', type=str, default="", help='Test LR to predict')
     parser.add_argument( '--target_rec', type=str, default="", help='Test LR to predict')
     parser.add_argument( '--remove_lig', type=str, default="False", help='Test LR to predict')
     parser.add_argument( '--remove_rec', type=str, default="False", help='Test LR to predict')
     parser.add_argument( '--remove_lrp', type=str, default="True", help='remove target LR pair from database')
     parser.add_argument( '--add_intra', type=int, default=-1, help='Set to 1 if you want to add intra network')
+    parser.add_argument( '--intra_cutoff', type=float, default=0.3 , help='?') 
     args = parser.parse_args()
     
+    args.remove_LR = [[args.target_lig, args.target_rec]]
+
     if args.remove_rec == "True" and args.target_rec == "":
         print("Please input args.target_rec, or set args.remove_rec=False")
         exit()
@@ -52,7 +57,7 @@ if __name__ == "__main__":
         exit()
 
     if args.neighborhood_threshold == 0:
-        args.neighborhood_threshold = args.spot_diameter*4
+        args.neighborhood_threshold = args.spot_diameter*args.num_hops
 
     if args.data_to == 'input_graph/':
         args.data_to = args.data_to + args.data_name + '/'
@@ -83,27 +88,40 @@ if __name__ == "__main__":
     
     else: # Data is not available in Space Ranger output format
         # read the mtx file
-        temp = sc.read_10x_mtx(args.data_from)
+        
+        temp = sc.read_10x_mtx(args.data_from) #
+        print(temp)
         print('*.mtx file read done')
-        gene_count_before = len(list(temp.var_names) )
+        gene_count_before = len(list(temp.var_names))
         sc.pp.filter_genes(temp, min_cells=args.filter_min_cell)
         gene_count_after = len(list(temp.var_names) )
         print('Gene filtering done. Number of genes reduced from %d to %d'%(gene_count_before, gene_count_after))
         gene_ids = list(temp.var_names) 
+        #print(len(gene_ids))
         cell_barcode = np.array(temp.obs.index)
+        temp = qnorm.quantile_normalize(np.transpose(sparse.csr_matrix.toarray(temp.X)))  #https://en.wikipedia.org/wiki/Quantile_normalization
+        cell_vs_gene = np.transpose(temp)
+        '''
+        temp = sc.read_mtx(args.data_from)
+        cell_vs_gene = sparse.csr_matrix.toarray(np.transpose(temp.X))
+        cell_barcode = pd.read_csv('../data/CID44971_spatial/filtered_count_matrix/barcodes.tsv.gz', header=None)
+        cell_barcode = np.array(list(cell_barcode[0]))
+        gene_ids = pd.read_csv('../data/CID44971_spatial/filtered_count_matrix/features.tsv.gz', header=None)
+        gene_ids = list(gene_ids[0])
+        print("Number of genes %d"%len(gene_ids))
         print('Number of barcodes: %d'%cell_barcode.shape[0])
         print('Applying quantile normalization')
-        temp = qnorm.quantile_normalize(np.transpose(sparse.csr_matrix.toarray(temp.X)))  #https://en.wikipedia.org/wiki/Quantile_normalization
+        temp = qnorm.quantile_normalize(np.transpose(cell_vs_gene))  #https://en.wikipedia.org/wiki/Quantile_normalization
         cell_vs_gene = np.transpose(temp)  
-    
+        '''
         
         # now read the tissue position file. It has the format:     
         df = pd.read_csv(args.tissue_position_file, sep=",", header=None)   
         tissue_position = df.values
         barcode_vs_xy = dict() # record the x and y coordinates for each spot/cell
         for i in range (0, tissue_position.shape[0]):
-            barcode_vs_xy[tissue_position[i][0]] = [tissue_position[i][4], tissue_position[i][5]] # x and y coordinates
-            #barcode_vs_xy[tissue_position[i][0]] = [tissue_position[i][5], tissue_position[i][4]] #for some weird reason, in the .h5 format for LUAD sample, the x and y are swapped
+            #barcode_vs_xy[tissue_position[i][0]] = [tissue_position[i][4], tissue_position[i][5]] # x and y coordinates
+            barcode_vs_xy[tissue_position[i][0]] = [tissue_position[i][5], tissue_position[i][4]] #for some weird reason, in the .h5 format for LUAD sample, the x and y are swapped
         
         coordinates = np.zeros((cell_barcode.shape[0], 2)) # insert the coordinates in the order of cell_barcodes
         for i in range (0, cell_barcode.shape[0]):
@@ -112,7 +130,11 @@ if __name__ == "__main__":
         
     
     
-    
+    #######################
+    if args.target_lig in gene_ids:
+        print('target ligand exist in the gene list')
+    if args.target_rec in gene_ids:
+        print('target rec exist in the gene list')
     ##################### make metadata: barcode_info ###################################
     i=0
     barcode_info=[]
@@ -182,9 +204,11 @@ if __name__ == "__main__":
             if ligand+'+'+receptor in target_LR_index:
                 continue
         if args.remove_lig == "True" and ligand in args.target_lig:
+            print('remove_lig true')
             continue
 
         if args.remove_rec == "True" and receptor in args.target_rec:
+            print('remove_rec true')
             continue
         
         
@@ -241,6 +265,7 @@ if __name__ == "__main__":
     
     # assign weight to the neighborhood relations based on neighborhood distance 
     dist_X = np.zeros((distance_matrix.shape[0], distance_matrix.shape[1]))
+    print(' --------- %g --------'%distance_matrix[1276][3517])
     for j in range(0, distance_matrix.shape[1]): # look at all the incoming edges to node 'j'
         max_value=np.max(distance_matrix[:,j]) # max distance of node 'j' to all it's neighbors (incoming)
         min_value=np.min(distance_matrix[:,j]) # min distance of node 'j' to all it's neighbors (incoming)
@@ -388,11 +413,11 @@ if __name__ == "__main__":
     gene_type_id = 0
     gene_type = dict()
 
-    if args.remove_lig == "True" and args.target_lig != "":
+    if (args.remove_lig == "True" or args.target_lig not in ligand_list) and args.target_lig != "":
         ligand_list.append(args.target_lig)
         print('adding ligand %s to ligand list'%args.target_lig)
 
-    if args.remove_rec == "True" and args.target_rec != "":
+    if (args.remove_rec == "True" or args.target_rec not in receptor_list) and args.target_rec != "":
         receptor_list.append(args.target_rec)
         print('adding receptor %s to receptor list'%args.target_rec)
         
@@ -489,7 +514,7 @@ if __name__ == "__main__":
                 if cell_vs_gene[spot_id][gene_index[gene_b]] < cell_percentile[spot_id]:
                     continue
 
-                if gene_coexpression_matrix[gene_a][gene_b] < 0.30:
+                if gene_coexpression_matrix[gene_a][gene_b] < args.intra_cutoff: #0.30:
                     continue
 
                 gene_b_idx = gene_node_list_per_spot[spot_id][gene_b]
@@ -565,7 +590,7 @@ if __name__ == "__main__":
         pickle.dump(barcode_info, fp)
 
     with gzip.open(args.metadata_to + args.data_name +'_barcode_info_gene', 'wb') as fp:  
-        pickle.dump([barcode_info_gene, ligand_list, receptor_list, gene_node_list_per_spot, dist_X, l_r_pair, gene_node_index_active], fp) #, ligand_active_count, rec_active_count
+        pickle.dump([barcode_info_gene, ligand_list, receptor_list, gene_node_list_per_spot, dist_X, l_r_pair, gene_node_index_active, ligand_active_count, rec_active_count], fp) #, ligand_active_count, rec_active_count
 
     with gzip.open(args.metadata_to + args.data_name +'_test_set', 'wb') as fp:  
         pickle.dump([target_LR_index, target_cell_pair], fp)
@@ -589,4 +614,6 @@ if __name__ == "__main__":
     ''' 
     print('write data done')
     
-# singularity run --home=/cluster/projects/schwartzgroup/fatema/nest_container  /cluster/projects/schwartzgroup/fatema/nest_container/nest_image.sif python LRbind_data_preprocess.py --data_from=/cluster/projects/schwartzgroup/fatema/data/V1_Human_Lymph_Node_spatial/ --data_name=LRbind_V1_Human_Lymph_Node_spatial_1D_manualDB_geneCorrLowWeight_remFromDB --database_path=database/NEST_database_no_predictedPPI.csv --split=16 --remove_lrp=True --remove_lig=True --remove_rec=True --target_lig=CCL19 --target_rec=CCR7
+# singularity run --home=/cluster/projects/schwartzgroup/fatema/nest_container  /cluster/projects/schwartzgroup/fatema/nest_container/nest_image.sif python LRbind_data_preprocess.py \
+    --data_from=/cluster/projects/schwartzgroup/fatema/data/V1_Human_Lymph_Node_spatial/ --data_name=LRbind_V1_Human_Lymph_Node_spatial_1D_manualDB_geneCorrLowWeight_remFromDB \
+    --database_path=database/NEST_database_no_predictedPPI.csv --split=16 --remove_lrp=True --remove_lig=True --remove_rec=True --target_lig=CCL19 --target_rec=CCR7
