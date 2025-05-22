96f28e09c4baeae6eda6757c1f02e63debcc7320
anne04 <anne.06.cse@gmail.com>
2025-05-22 11:59:15 -0400

    Update and rename read_parafold_json.py to LRbind_plot_AlphaFold_scores.py

--- LRbind_plot_AlphaFold_scores.py
+++ LRbind_plot_AlphaFold_scores.py
@@ -6,8 +6,7 @@ import seaborn as sns
 
 
 score_list = []
-file_path = 'ParallelFold-main/output/lrbind_SERPING1_NCL_score.json'
-file_list = glob.glob("ParallelFold-main/output/manual_*json")
+file_list = glob.glob("ParallelFold-main/output/manual*json")
 for file_path in file_list:
     with open(file_path, 'r') as file:
         data = json.load(file)
@@ -22,6 +21,24 @@ plt.xlabel('AlphaFold Score')
 plt.ylabel('Frequency')
 plt.savefig('/cluster/home/t116508uhn/LRbind_output/AF_score_distribution_manualLRP_'+str(len(score_list))+'LRP.jpg')
 
+
+##############
+
+score_list = []
+file_list = glob.glob("ParallelFold-main/output/lrb*json")
+for file_path in file_list:
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    
+    score_list.append(data['iptm+ptm']['model_1_multimer_v3_pred_0'])
+
+
+plt.clf()
+sns.histplot(score_list, bins=30, kde=True, color='skyblue') #kde adds a kernel density estimate
+plt.title('AlphaFold score distribution for predicted LRP')
+plt.xlabel('AlphaFold Score')
+plt.ylabel('Frequency')
+plt.savefig('/cluster/home/t116508uhn/LRbind_output/AF_score_distribution_predictedLRP_'+str(len(score_list))+'LRP.jpg')
 
 
 
