e5ccbb90f33bdfe4db1dcdb1069b0a318673e08a
anne04 <anne.06.cse@gmail.com>
2025-05-23 10:22:31 -0400

    Update LRbind_plot_AlphaFold_scores.py

--- LRbind_plot_AlphaFold_scores.py
+++ LRbind_plot_AlphaFold_scores.py
@@ -23,24 +23,34 @@ plt.savefig('/cluster/home/t116508uhn/LRbind_output/AF_score_distribution_manual
 
 
 ##############
-
+output_path = '/cluster/home/t116508uhn/LRbind_output/'
+file_name = 'AF_score_distribution_manualLRP_' #'AF_score_distribution_predictedLRP_'
+lr_type =  'manual' #'lrbind'
+lrpair_score_dict = defaultdict(list)
 score_list = []
-file_list = glob.glob("ParallelFold-main/output/lrb*json")
+file_list = glob.glob("ParallelFold-main/output/"+ lr_type +"*json")
 for file_path in file_list:
+    if '_old_' in file_path:
+        continue
     with open(file_path, 'r') as file:
         data = json.load(file)
-    
-    score_list.append(data['iptm+ptm']['model_1_multimer_v3_pred_0'])
 
+    AF_score = data['iptm+ptm']['model_1_multimer_v3_pred_0']
+    score_list.append(AF_score)
+    ligand = file_path.split('_')[1]
+    receptor = file_path.split('_')[2]
+    lrpair_score_dict['pair'].append(ligand + '_to_' + receptor)
+    lrpair_score_dict['AF scores'].append(AF_score)
 
 plt.clf()
 sns.histplot(score_list, bins=30, kde=True, color='skyblue') #kde adds a kernel density estimate
 plt.title('AlphaFold score distribution for predicted LRP')
 plt.xlabel('AlphaFold Score')
 plt.ylabel('Frequency')
-plt.savefig('/cluster/home/t116508uhn/LRbind_output/AF_score_distribution_predictedLRP_'+str(len(score_list))+'LRP.jpg')
+plt.savefig(output_path + file_name +str(len(score_list))+'LRP.jpg')
 
-
+data_list_pd = pd.DataFrame(lrpair_score_dict)
+data_list_pd.to_csv(output_path + file_name +str(len(score_list))+'LRP.csv' , index=False)
 
 # Print the data
 print(data)
