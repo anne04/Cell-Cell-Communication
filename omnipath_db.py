82da79808e85edd9df910a4d2b971f9b997d00a5
anne04 <anne.06.cse@gmail.com>
2025-07-22 22:18:34 -0400

    Create omnipath_db.py

---
+++ omnipath_db.py
@@ -0,0 +1,13 @@
+# https://workflows.omnipathdb.org/intercell-networks-py.html
+import omnipath as op
+import pandas as pd
+
+
+intercell = op.interactions.import_intercell_network()
+
+intercell_filtered = intercell[
+    (intercell['category_intercell_source'] == 'ligand' & intercell['category_intercell_source'] == 'secreted' ) & # set transmitters to be ligands
+    (intercell['category_intercell_target'] == 'receptor') # set receivers to be receptors
+]
+intercell_filtered
+intercell_filtered.to_csv('omnipath_lr_db.csv', index=False)
