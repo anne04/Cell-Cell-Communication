6d1147f0fdf87f3c741daea7b53bad18bbb8c6b3
anne04 <anne.06.cse@gmail.com>
2025-07-22 22:51:03 -0400

    Update negatome_db_read.py

--- negatome_db_read.py
+++ negatome_db_read.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     parser.add_argument( '--file_name', type=str, default='uniprotkb_reviewed_true_AND_proteome_up_2025_02_27.tsv', help='The name of DB')
     parser.add_argument( '--negatome_database_path', type=str, default='database/combined_stringent.txt', help='The name of DB')
     parser.add_argument( '--database_path', type=str, default='database/NEST_database_no_predictedPPI.csv', help='The name of DB')
+    parser.add_argument( '--database_path_omnipath', type=str, default='database/omnipath_lr_db.csv', help='The name of DB')
     parser.add_argument( '--result_path', type=str, default='result/')
     args = parser.parse_args()
 
@@ -34,19 +35,21 @@ if __name__ == "__main__":
             dict_uniprotID_genes[uniprotID] = gene_list      
             
 
-    ############################
-    df = pd.read_csv(args.database_path, sep=",")
+
+    ##############################
+    df = pd.read_csv(args.database_path_omnipath, sep=",")
     #lr_unique = defaultdict(dict)
     ligand_list = []
     receptor_list = []
-    for i in range (0, df["Ligand"].shape[0]):
-        ligand = df["Ligand"][i]
-        receptor = df["Receptor"][i]
+    for i in range (0, df['genesymbol_intercell_source'].shape[0]):
+        ligand = df['genesymbol_intercell_source'][i]
+        receptor = df['genesymbol_intercell_target'][i]
+        if str(df['secreted_intercell_source'][i]) == 'False' or str(df['is_inhibition'][i]) =='True' :
+            continue
+            
         #lr_unique[ligand][receptor] = 1
         ligand_list.append(ligand)
         receptor_list.append(receptor)
-
-    ##############################
         
 
     ##################################################################
@@ -73,6 +76,41 @@ if __name__ == "__main__":
                     lr_unique[gene_b][gene_a] = 1
         
 
+    ############################
+    df = pd.read_csv(args.database_path, sep=",")
+    #lr_unique = defaultdict(dict)
+    ligand_list = []
+    receptor_list = []
+    for i in range (0, df["Ligand"].shape[0]):
+        ligand = df["Ligand"][i]
+        receptor = df["Receptor"][i]
+        #lr_unique[ligand][receptor] = 1
+        ligand_list.append(ligand)
+        receptor_list.append(receptor)
+
+
+    df = pd.read_csv(args.negatome_database_path, sep="\t", header=None)
+    for i in range (0, df[0].shape[0]):
+        ligand_uniprot = df[0][i]
+        receptor_uniprot = df[1][i]
+        chain_A_list = dict_uniprotID_genes[ligand_uniprot]
+        chain_B_list = dict_uniprotID_genes[receptor_uniprot]
+
+        for gene_a in chain_A_list:
+            for gene_b in chain_B_list:
+                if gene_a in ligand_list and gene_b in receptor_list:
+                    negatome_ligand_list.append(gene_a)
+                    negatome_receptor_list.append(gene_b)
+                    lr_unique[gene_a][gene_b] = 1
+
+                if gene_b in ligand_list and gene_a in receptor_list:
+                    negatome_ligand_list.append(gene_b)
+                    negatome_receptor_list.append(gene_a)
+                    lr_unique[gene_b][gene_a] = 1
+        
+
+    
+    
     
     negatome_ligand_list = list(np.unique(negatome_ligand_list))
     negatome_receptor_list = list(np.unique(negatome_receptor_list))
@@ -82,10 +120,11 @@ if __name__ == "__main__":
     	pickle.dump([negatome_ligand_list, negatome_receptor_list, lr_unique], fp)
 
 ####
+i = 0
 for ligand_gene in lr_unique:
     for rec_gene in lr_unique[ligand_gene]:
-        print(ligand_gene + '_to_' + rec_gene)
-
+        print(str(i)+' - '+ligand_gene + '_to_' + rec_gene)
+        i = i+1
 
         
         
