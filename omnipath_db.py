456d603a934d9339aa625fe8328fb6ff0f4628b1
anne04 <anne.06.cse@gmail.com>
2025-07-23 10:25:43 -0400

    Update omnipath_db.py

--- omnipath_db.py
+++ omnipath_db.py
@@ -6,7 +6,7 @@ import pandas as pd
 intercell = op.interactions.import_intercell_network()
 
 intercell_filtered = intercell[
-    (intercell['category_intercell_source'] == 'ligand' & intercell['category_intercell_source'] == 'secreted' ) & # set transmitters to be ligands
+    (intercell['category_intercell_source'] == 'ligand'  | intercell['category_intercell_source'] == 'secreted' ) & # set transmitters to be ligands
     (intercell['category_intercell_target'] == 'receptor') # set receivers to be receptors
 ]
 intercell_filtered
