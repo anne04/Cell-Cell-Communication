a9b08b75f148899e1c036c695d524f41b22244ad
anne04 <anne.06.cse@gmail.com>
2025-08-05 12:57:10 -0400

    Create run_embFusion.py

---
+++ run_embFusion.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import numpy as np
+from datetime import datetime 
+import time
+import random
+import argparse
+import torch
+from embFusion import data_to_tensor
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # =========================== must be provided ===============================
+    #parser.add_argument( '--data_name', type=str, help='Name of the dataset') #default='PDAC_64630', 
+    parser.add_argument( '--model_name', type=str, help='Provide a model name')
+    #=========================== default is set ======================================
+    parser.add_argument( '--num_epoch', type=int, default=60000, help='Number of epochs or iterations for model training')
+    parser.add_argument( '--model_path', type=str, default='model/', help='Path to save the model state') # We do not need this for output generation  
+    parser.add_argument( '--training_data', type=str, default='input_graph/', help='Path to input graph. ')
+    parser.add_argument( '--dropout', type=float, default=0)
+    parser.add_argument( '--lr_rate', type=float, default=0.00001)
+    parser.add_argument( '--manual_seed', type=str, default='no')
+    parser.add_argument( '--seed', type=int )
+    #=========================== optional ======================================
+    parser.add_argument( '--load', type=int, default=0, help='Load a previously saved model state')  
+    parser.add_argument( '--load_model_name', type=str, default='None' , help='Provide the model name that you want to reload')
+    #============================================================================
+    args = parser.parse_args() 
+
+    args.training_data = args.training_data #+ args.data_name + '/' + args.data_name + '_' + 'adjacency_records'
+
+    
+    args.model_path = args.model_path +'/'
+    args.model_name = args.model_name 
+
+
+    if args.manual_seed == 'yes':
+        torch.manual_seed(args.seed)
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+
+    if not os.path.exists(args.model_path):
+        os.makedirs(args.model_path) 
+
+    print ('------------------------Model and Training Details--------------------------')
+    print(args) 
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    
+    with gzip.open('database/'+'LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir'+'_dataset_embFusion.pkl', 'rb') as fp:  
+    	dataset = pickle.load(fp)
+
+    # shuffle and keep 80% for training, 20% for validation
+    train_dataset = dataset[0:(len(dataset)*80)//100]
+    val_dataset = dataset[(len(dataset)*80)//100:]
+
+    train_dataset_tensor = data_to_tensor(train_dataset)
+    val_dataset_tensor = data_to_tensor(val_dataset)
+
+    
+
+
+    
