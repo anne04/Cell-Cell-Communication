275e7f15c02936bf313e0afc6d1148abc979072c
anne04 <anne.06.cse@gmail.com>
2025-08-14 10:42:41 -0400

    Update get_prediction_by_embFusion.py

--- get_prediction_by_embFusion.py
+++ get_prediction_by_embFusion.py
@@ -17,7 +17,7 @@ def val_fusionMLP_multiBatch(dataset, model_name, threshold_score = 0.7, total_b
     # initialize the model
     """
     model_fusionMLP = fusionMLP(
-                 input_size = 512 + 264 + 1024, 
+                 input_size = 264 + 1024, 
                  hidden_size_fusion = 1024, 
                  output_size_fusion = 256,
                  hidden_size_predictor_layer1 = 256*2,
@@ -75,39 +75,21 @@ def val_fusionMLP_multiBatch(dataset, model_name, threshold_score = 0.7, total_b
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # =========================== must be provided ===============================
-    #parser.add_argument( '--data_name', type=str, help='Name of the dataset') #default='PDAC_64630', 
-    parser.add_argument( '--model_name', type=str, default="embFusion_test", help='Provide a model name')
+    parser.add_argument( '--model_name', type=str, default="embFusion_gene_prot", help='Provide a model name')
+    parser.add_argument( '--model_path', type=str, default='embFusion_model', help='Path to save the model state') # We do not need this for output generation 
     parser.add_argument( '--lr_lrbind_csv_path', type=str, 
                         default='/cluster/home/t116508uhn/LRbind_output/without_elbow_cut/LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir/model_LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir_3L_allLR_nodeInfo.csv', 
                         help='Name of the dataset') #, required=True)
     #=========================== default is set ======================================
-    parser.add_argument( '--num_epoch', type=int, default=10000, help='Number of epochs or iterations for model training')
-    parser.add_argument( '--model_path', type=str, default='model/', help='Path to save the model state') # We do not need this for output generation  
-    parser.add_argument( '--training_data', type=str, default='database/LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir_dataset_embFusion.pkl', help='Path to input graph. ')
-    parser.add_argument( '--dropout', type=float, default=0)
-    parser.add_argument( '--lr_rate', type=float, default=0.00001)
-    parser.add_argument( '--manual_seed', type=str, default='no')
-    parser.add_argument( '--seed', type=int )
-    #=========================== optional ======================================
-    parser.add_argument( '--load', type=int, default=0, help='Load a previously saved model state')  
-    parser.add_argument( '--load_model_name', type=str, default='None' , help='Provide the model name that you want to reload')
+    parser.add_argument( '--data_to_predict', type=str, \
+                    default='/cluster/projects/schwartzgroup/fatema/LRbind/database/LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir_dataset_results_to_embFusion.pkl', \
+                    help='Path to input graph. ')
     #============================================================================
     args = parser.parse_args() 
 
-    args.training_data = args.training_data #+ args.data_name + '/' + args.data_name + '_' + 'adjacency_records'
 
-    
     args.model_path = args.model_path +'/'
-    args.model_name = args.model_name 
-
-
-    if args.manual_seed == 'yes':
-        torch.manual_seed(args.seed)
-        random.seed(args.seed)
-        np.random.seed(args.seed)
-
-    if not os.path.exists(args.model_path):
-        os.makedirs(args.model_path) 
+    args.model_name = args.model_path + args.model_name 
 
     print ('------------------------Model and Training Details--------------------------')
     print(args) 
@@ -115,13 +97,13 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
     ccc_pairs = pd.read_csv(args.lr_lrbind_csv_path, sep=",")
-    with gzip.open('database/'+'LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir'+'_dataset_results_to_embFusion.pkl', 'rb') as fp:  
+    with gzip.open(args.data_to_predict, 'rb') as fp:  
     	dataset, record_index = pickle.load(fp)
 
     
    
 
-    model_name = 'model/my_model_fusionMLP.pickle'
+    model_name = args.model_name
     
     #val_set, na = data_to_tensor(dataset, None)
     prediction_score, pred_class = val_fusionMLP_multiBatch(dataset, model_name, threshold_score=0.7, total_batch = 1000)    
@@ -142,7 +124,7 @@ if __name__ == "__main__":
     # now add this column to ccc_pairs
     ccc_pairs['pred_score'] = pred_score
     # save it
-    ccc_pairs.to_csv(args.lr_lrbind_csv_path, index=False) 
+    ccc_pairs.to_csv('model_LRbind_LUAD_1D_manualDB_geneCorrP7KNN_bidir_3L_allLR_nodeInfo.csv', index=False) 
 
 
 
